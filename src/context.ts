2cba23324bed76739d6539c2e6e125649535d1c0
Felipe Escoto Aceves <feescoto@microsoft.com>
2020-07-09 17:38:31 -0700

    Move profiler output to the system tmp dir

--- src/context.ts
+++ src/context.ts
@@ -1,21 +1,27 @@
 import { Config } from "./types/Config";
 import { RunContext } from "./types/RunContext";
 import Profiler from "p-profiler";
+import { join } from "path";
+import { tmpdir } from "os";
+import { mkdirSync } from 'fs';
 
 export function createContext(config: Pick<Config, "concurrency">): RunContext {
   const { concurrency } = config;
+
+  const profilerOutputDir = join(tmpdir(), "lage", "profiles");
+  mkdirSync(profilerOutputDir, { recursive: true });
 
   return {
     measures: {
       start: [0, 0],
       duration: [0, 0],
       taskStats: [],
-      failedTask: undefined,
+      failedTask: undefined
     },
     profiler: new Profiler({
       concurrency,
       prefix: "lage",
-      outDir: process.cwd(),
-    }),
+      outDir: profilerOutputDir
+    })
   };
 }
