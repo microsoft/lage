ad594ea20610a6a06a64fa56d4e44d93003e21c0
Ken Chau <kchau@microsoft.com>
2020-07-22 16:48:28 -0700

    adding an init interface

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,28 @@
-import { createContext } from "./context";
 import { getConfig } from "./config/getConfig";
-import { getWorkspace } from "./workspace/getWorkspace";
-import { parseArgs, validateInput } from "./args";
-import { reportSummary } from "./logger/reportSummary";
-import { runTasks } from "./task/taskRunner";
-import { logLevel, logger } from "./logger";
-import { generateTopologicGraph } from "./workspace/generateTopologicalGraph";
-import { signal } from "./task/abortSignal";
-import { killAllActiveProcesses } from "./task/npmTask";
+import { logLevel } from "./logger";
+import { init } from "./command/init";
+import { run } from "./command/run";
+import { showHelp } from "./showHelp";
 
-console.log(`🧱 Lage task runner 🧱`);
+console.log(`🔨 Lage task runner - let's make it`);
 console.log(``);
 
 // Parse CLI args
-const parsedArgs = parseArgs();
-validateInput(parsedArgs);
-
-// Create context
 const cwd = process.cwd();
-const config = getConfig(cwd);
-const context = createContext(config);
-const workspace = getWorkspace(cwd, config);
+try {
+  const config = getConfig(cwd);
 
-// Initialize logger
-if (config.verbose) {
-  logLevel("verbose");
-}
-
-// generate topological graph
-const graph = generateTopologicGraph(workspace);
-
-(async () => {
-  const { profiler } = context;
-  context.measures.start = process.hrtime();
-
-  // die faster if an abort signal is seen
-  signal.addEventListener("abort", () => {
-    killAllActiveProcesses();
-    displayReportAndExit();
-  });
-
-  try {
-    await runTasks({ graph, workspace, context, config });
-  } catch (e) {
-    logger.error("runTasks", e);
+  // Initialize logger
+  if (config.verbose) {
+    logLevel("verbose");
   }
 
-  if (config.profile) {
-    const profileFile = profiler.output();
-    logger.info("runTasks", `Profile saved to ${profileFile}`);
+  if (config.command[0] === "init") {
+    init(cwd);
+  } else {
+    run(cwd, config);
   }
-
-  displayReportAndExit();
-})();
-
-function displayReportAndExit() {
-  context.measures.duration = process.hrtime(context.measures.start);
-  reportSummary(context);
-  if (context.measures.failedTask) {
-    process.exit(1);
-  }
+} catch (e) {
+  console.error(e);
+  showHelp(e.message);
 }
