25e85fa9a97d782fc06fd39a88a24fc9c64a3641
Ken Chau <kchau@microsoft.com>
2020-11-30 11:45:11 -0800

    on the safe side, we make sure we "over" build if git diff fails

--- src/types/Workspace.ts
+++ src/types/Workspace.ts
@@ -3,7 +3,6 @@ import { PackageInfos } from "workspace-tools";
 export interface Workspace {
   root: string;
   allPackages: PackageInfos;
-  changedPackages: string[];
   npmClient: string;
   npmCmd: string;
 }
