1e2efdd4cb426f0938d5df379b5856c723e100fd
Ken Chau <kchau@microsoft.com>
2021-07-07 15:17:00 -0700

    reuse workerQueue

--- src/task/workerQueue.ts
+++ src/task/workerQueue.ts
@@ -1,7 +1,15 @@
 import Queue from "bee-queue";
+import { worker } from "../command/worker";
 import { Config } from "../types/Config";
 
 const queueId = `lage:npm-task:${process.env.LAGE_WORKER_QUEUE_ID || "default"}`;
 
-export const getWorkerQueue = (config: Config["workerQueueOptions"], isWorker: boolean = true) =>
-  new Queue(queueId, { ...config, isWorker });
+let workerQueue: Queue;
+
+export const getWorkerQueue = (config: Config["workerQueueOptions"], isWorker: boolean = true) => {
+  if (!workerQueue) {
+    workerQueue = new Queue(queueId, { ...config, isWorker });
+  }
+
+  return workerQueue;
+};
