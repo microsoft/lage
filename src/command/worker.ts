f77c2f7da50d692dc598d706937ad9ea1c88280c
Ken Chau <kchau@microsoft.com>
2021-07-08 14:10:22 -0700

    different exit condition

--- src/command/worker.ts
+++ src/command/worker.ts
@@ -10,89 +10,95 @@ import { TaskLogger } from "../logger/TaskLogger";
 import { cacheFetch, cacheHash, cachePut } from "../cache/backfill";
 import { getTaskId } from "@microsoft/task-scheduler";
 
-
 // Run multiple
 export async function worker(cwd: string, config: Config, reporters: Reporter[]) {
   const workspace = getWorkspace(cwd, config);
   const workerQueue = await initWorkerQueue(config.workerQueueOptions);
 
-  workerQueue.on('job succeeded', async() => {
-    const jobs = await workerQueue.getJobs('waiting', {size: 1})
-    if (jobs.length === 0) {
-      workerQueue.close();
-    }
-  });
-  
-  workerQueue.process(config.concurrency, async (job, done) => {
-    console.log(`processing job ${job.id}: ${job.data.name} ${job.data.task}`);
-
-    await Promise.all(
-      job.data.taskDeps.map((taskDep: string) => {
-        const [name, task] = taskDep.split("#");
-
-        if (task) {
-          const info = workspace.allPackages[name];
-          const packagePath = path.dirname(info.packageJsonPath);
-          return getCache({
-            task,
-            name,
-            root: cwd,
-            packagePath,
-            config,
-          });
-        }
-      })
-    );
-
-    const cacheResult = await getCache({
-      task: job.data.task,
-      name: job.data.name,
-      root: cwd,
-      packagePath: path.join(cwd, job.data.packagePath),
-      config,
-    });
-
-    if (cacheResult.cacheHit) {
-      return done();
+  workerQueue.ready(() => {
+    async function checkActive() {
+      const counts = await workerQueue.checkHealth();
+      const isRunning = (counts.active + counts.delayed + counts.waiting) > 0;
+      if (!isRunning) {
+        workerQueue.close();
+      } else {
+        setTimeout(checkActive, 2000);
+      }
     }
 
-    const { npmArgs, spawnOptions, packagePath } = job.data;
-    const npmCmd = findNpmClient(config.npmClient);
+    checkActive();
 
-    const cp = spawn(npmCmd, npmArgs, {
-      cwd: path.join(cwd, packagePath),
-      ...spawnOptions,
-    });
+    workerQueue.process(config.concurrency, async (job, done) => {
+      console.log(`processing job ${job.id}: ${job.data.name} ${job.data.task}`);
 
-    const logger = new TaskLogger(job.data.name, job.data.task);
+      await Promise.all(
+        job.data.taskDeps.map((taskDep: string) => {
+          const [name, task] = taskDep.split("#");
 
-    const stdoutLogger = new TaskLogWritable(logger);
-    cp.stdout.pipe(stdoutLogger);
+          if (task) {
+            const info = workspace.allPackages[name];
+            const packagePath = path.dirname(info.packageJsonPath);
+            return getCache({
+              task,
+              name,
+              root: cwd,
+              packagePath,
+              config,
+            });
+          }
+        })
+      );
 
-    const stderrLogger = new TaskLogWritable(logger);
-    cp.stderr.pipe(stderrLogger);
-
-    cp.on("exit", async () => {
-      await saveCache(cacheResult.hash, {
+      const cacheResult = await getCache({
         task: job.data.task,
         name: job.data.name,
         root: cwd,
-        packagePath: job.data.packagePath,
+        packagePath: path.join(cwd, job.data.packagePath),
         config,
       });
 
-      done();
+      if (cacheResult.cacheHit) {
+        return done();
+      }
+
+      const { npmArgs, spawnOptions, packagePath } = job.data;
+      const npmCmd = findNpmClient(config.npmClient);
+
+      const cp = spawn(npmCmd, npmArgs, {
+        cwd: path.join(cwd, packagePath),
+        ...spawnOptions,
+      });
+
+      const logger = new TaskLogger(job.data.name, job.data.task);
+
+      const stdoutLogger = new TaskLogWritable(logger);
+      cp.stdout.pipe(stdoutLogger);
+
+      const stderrLogger = new TaskLogWritable(logger);
+      cp.stderr.pipe(stderrLogger);
+
+      cp.on("exit", async () => {
+        await saveCache(cacheResult.hash, {
+          task: job.data.task,
+          name: job.data.name,
+          root: cwd,
+          packagePath: job.data.packagePath,
+          config,
+        });
+
+        done();
+      });
     });
   });
 }
 
 // speeding up to reduce network costs for a worker
-const localHashCache: {[packageTask: string]: string} = {};
+const localHashCache: { [packageTask: string]: string } = {};
 
 async function getCache(options: any) {
   let hash: string | null = null;
   let cacheHit = false;
-  
+
   const { task, name, root, packagePath, config } = options;
 
   const localCacheKey = getTaskId(name, task);
