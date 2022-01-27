f2deecf7891c6d1fe0af9028dd0347f1a5c8e65b
Ken <kchau@microsoft.com>
2022-01-27 13:40:53 -0800

    added a test

--- tests/e2e/remoteFallback.test.ts
+++ tests/e2e/remoteFallback.test.ts
@@ -1,8 +1,70 @@
 import { Monorepo } from "../mock/monorepo";
 import { parseNdJson } from "./parseNdJson";
 
-// WIP!!
 describe("RemoteFallbackCacheProvider", () => {
+  it("should skip local cache population if --skip-local-cache is enabled", () => {
+    const repo = new Monorepo("fallback");
+
+    repo.init();
+    repo.setLageConfig(
+      `const fs = require('fs');
+      const path = require('path');
+      module.exports = {
+        pipeline: {
+          build: [],
+        },
+        cache: true,
+        cacheOptions: {
+          writeRemoteCache: true,
+          cacheStorageConfig: {
+            provider: (logger, cwd) => ({
+              async fetch(hash) {
+                return false;
+              },
+
+              async put(hash, filesToCache) {
+              },
+            }),
+          },
+        } 
+      };`
+    );
+
+    repo.install();
+
+    repo.addPackage("a", [], {
+      build: "echo a:build",
+      test: "echo a:test",
+    });
+    repo.addPackage("b", [], {
+      build: "echo b:build",
+    });
+    repo.linkPackages();
+
+    const results = repo.run("test", ["--verbose", "--skip-local-cache"]);
+
+    const output = results.stdout + results.stderr;
+    const jsonOutput = parseNdJson(output);
+
+    expect(
+      jsonOutput.find((entry) => entry.msg?.includes("local cache fetch"))
+    ).toBeFalsy();
+
+    expect(
+      jsonOutput.find((entry) => entry.msg?.includes("remote fallback fetch"))
+    ).toBeTruthy();
+
+    expect(
+      jsonOutput.find((entry) => entry.msg?.includes("local cache put"))
+    ).toBeFalsy();
+
+    expect(
+      jsonOutput.find((entry) => entry.msg?.includes("remote fallback put"))
+    ).toBeTruthy();
+
+    repo.cleanup();
+  })
+
   it("should operate with local provider ONLY by default", () => {
     const repo = new Monorepo("fallback");
 
