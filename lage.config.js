c0d69c29e1ac1f315e1f91139ec1c4bde15561dd
Kenneth Chau <34725+kenotron@users.noreply.github.com>
2022-05-25 13:05:03 -0700

    Making this repo into a monorepo in preparation of more granular work (#250)
    * Turning lage into monorepo to prepare subdividing into pieces
    
    * getting rid of renovate for now
    
    * updated the root scripts to work
    
    * fixing up pipelines
    
    * moving many files around requires adjustments to beachball config
    
    * upgrade node version to allow docusaurus to consume newer node version
    
    * Change files
    
    * get rid of the extra docs: prefix
    
    * adding verbose logging

---
+++ lage.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/** @type {import("lage").ConfigOptions} */
+module.exports = {
+  pipeline: {
+    "build": ["^build"],
+    "test": [],
+    "lint": [],
+    "start": []
+  },
+  npmClient: "yarn"
+}
