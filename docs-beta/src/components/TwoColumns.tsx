1a109e6ba8f932929cf8958ec46da8369150dd6a
Bilge Deniz Koçak <85181818+BilgeDenizKocakk@users.noreply.github.com>
2022-06-06 11:00:44 -0700

    Added testimonials, tweets, tools, and a table to the landing page. (#256)
    * Added testimonials, tweets, tools, and a table to the landing page.
    
    * Delete package-lock.json
    
    deleting since this file is automatically generated.
    
    * Delete yarn.lock
    
    * Update Features.js
    
    getting rid of an unnecessary import statement
    
    * Update Footer.js
    
    getting rid of an unnecessary import statement
    
    * Update Footer.js
    
    * Update Header.js
    
    getting rid of an unnecessary import statement
    
    * Update SideBySide.js
    
    getting rid of an unnecessary import statement
    
    * Update Table.js
    
    getting rid of an unnecessary import statement
    
    * Update Testimonials.js
    
    getting rid of an unnecessary import statement
    
    * Update Tools.js
    
    getting rid of an unnecessary import statement
    
    * Update Tweet.js
    
    getting rid of an unnecessary import statement
    
    * Update TwoColumns.js
    
    getting rid of an unnecessary import statement
    
    * Update TwoColumns.js
    
    * Delete package-lock.json
    
    * Update package.json
    
    * style, custom CSS color, restructuring data
    
    * new data structure for table, writing a unit test
    
    * getting rid of yarn.lock file
    
    * changing the script for running docs-beta
    
    * adding yarn.lock file back
    
    * tests doesn't pass, but it runs!
    
    * moving to ts
    
    * deleting unnecessary dependencies
    
    * deleting unnecessary dependencies within docs
    
    * yarn file matches original docs
    
    Co-authored-by: Bilge Kocak <t-bilgekocak@microsoft.com>
    Co-authored-by: Ken Chau <kchau@microsoft.com>

---
+++ docs-beta/src/components/TwoColumns.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function TwoColumns({className = "", children}) {
+    return (
+        <div className={`mx-auto ${className}`}>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+        {React.Children.map(children, (child, i) => {
+            if (i < 2) return <div className="flex float-left py-4 align-center">{child}</div>
+            else return
+            })}
+        </div>
+        </div>
+    );
+}
