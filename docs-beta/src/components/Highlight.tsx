ec2ec517c533819667e159156467eebebdbdb52d
Bilge Deniz Kocak <bkocak1@villanova.edu>
2022-06-15 05:24:25 -0700

    centering highlights

--- docs-beta/src/components/Highlight.tsx
+++ docs-beta/src/components/Highlight.tsx
@@ -4,7 +4,7 @@ export const Highlight = (props) => {
   return (
     <div>
       <div className="flex items-center h-20 bg-white mt-4 mx-4 md:mx-8 rounded-r-full rounded-l-full">
-        <p className="font-bahnschrift font-bold text-lg text-body-primary px-4 mx-auto">
+        <p className="font-bahnschrift font-bold text-lg text-body-primary px-4 mx-auto text-center">
           {props.children}
         </p>
       </div>
