d2221764e5306bcb95949a634ba6302535224f65
Bilge Deniz Kocak <bkocak1@villanova.edu>
2022-07-21 12:08:01 -0700

    formatting with prettier

--- packages/lage/src/command/graph.ts
+++ packages/lage/src/command/graph.ts
@@ -1,15 +1,9 @@
-import {
-  getWorkspaceRoot,
-  getPackageInfos,
-  createPackageGraph,
-} from "workspace-tools";
+import { getWorkspaceRoot, getPackageInfos, createPackageGraph } from "workspace-tools";
 
 export async function graph(cwd: string) {
   const root = getWorkspaceRoot(cwd);
   if (!root) {
-    throw new Error(
-      "This must be called inside a codebase that is part of a JavaScript workspace."
-    );
+    throw new Error("This must be called inside a codebase that is part of a JavaScript workspace.");
   }
   const allPackages = getPackageInfos(root);
   const edges = createPackageGraph(allPackages, {
