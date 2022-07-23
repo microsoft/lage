377946380ef6461f786004904adeefc68070bf09
Ken Chau <ken@gizzar.com>
2022-07-23 10:47:34 -0700

    adding jsdocs for the target config

--- packages/target-graph/src/types/TargetConfig.ts
+++ packages/target-graph/src/types/TargetConfig.ts
@@ -1,18 +1,64 @@
 import type { Target } from "./Target";
 
-/** target configuration */
+/** 
+ * Target configuration - to be used inside `lage.config.js` options.pipeline configurations
+ */
 export interface TargetConfig {
+  /**
+   * @deprecated
+   * The type of the target - The configuration parser will use the id of the target to determine the type
+   * 
+   * This is a legacy way of specifying the type of the target, to be deprecated in the next major version
+   */
   type?: "package" | "global";
 
-  /** @deprecated */
+  /** 
+   * @deprecated - use `dependsOn` instead 
+   * 
+   * The dependencies of the target. Dependencies are target specs in one of these forms:
+   * - "pkg-a#build"
+   * - "build"
+   * - "^build"
+   * - "^^build"
+   */
   deps?: string[];
+
+  /** 
+   * The dependencies of the target. Dependencies are target specs in one of these forms:
+   * - "pkg-a#build"
+   * - "build"
+   * - "^build"
+   * - "^^build"
+   */
   dependsOn?: string[];
 
+  /**
+   * Inputs for targets. This is used to determine the hash key for caching
+   */
   inputs?: string[];
+
+  /**
+   * Outputs for targets. This is used to determine the files to be stored for caching
+   */
   outputs?: string[];
+
+  /**
+   * Priority of the target. A priority of >0 will always be prioritized over the default targets in queue
+   */
   priority?: number;
+
+  /**
+   * Whether to cache this target (defaults to true)
+   */
   cache?: boolean;
+
+  /**
+   * Run options for the Target. (e.g. `{ env: ...process.env, colors: true, ... }`)
+   */
   options?: Record<string, any>;
 
+  /**
+   * Custom run definition, if left blank, the scheduler will decide which runner to use to fulfill the work for the `Target`
+   */
   run?: (target: Target) => Promise<void> | void;
 }
