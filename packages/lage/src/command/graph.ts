6cb9715b828b242cb6c5bee05ea3624cfef12d8d
Bilge Deniz Kocak <bkocak1@villanova.edu>
2022-07-23 15:33:06 -0700

    returns the entire graph for no scope

--- packages/lage/src/command/graph.ts
+++ packages/lage/src/command/graph.ts
@@ -1,6 +1,8 @@
 import { Config } from "../types/Config";
 import { getWorkspaceRoot, getPackageInfos, createPackageGraph } from "workspace-tools";
 import { getConfig } from "../config/getConfig";
+import { getWorkspace } from "../workspace/getWorkspace";
+import { getPipelinePackages } from "../task/getPipelinePackages";
 
 export async function graph(cwd: string, config: Config) {
   const fs = require("fs");
@@ -8,14 +10,29 @@ export async function graph(cwd: string, config: Config) {
   if (!root) {
     throw new Error("This must be called inside a codebase that is part of a JavaScript workspace.");
   }
+  const workspace = getWorkspace(cwd, config);
+  const packages = getPipelinePackages(workspace, config);
   const allPackages = getPackageInfos(root);
   const pattern = getConfig(cwd).scope;
-  const edges = createPackageGraph(allPackages, {
-    namePatterns: pattern,
-    includeDependencies: config.includeDependencies,
-    includeDependents: config.includeDependents,
-    withDevDependencies: config.withDevDependencies,
-    withPeerDependencies: config.withPeerDependencies,
-  });
+  var edges;
+
+  if (pattern.length != 0) {
+    edges = createPackageGraph(allPackages, {
+      namePatterns: pattern,
+      includeDependencies: config.includeDependencies,
+      includeDependents: config.includeDependents,
+      withDevDependencies: config.withDevDependencies,
+      withPeerDependencies: config.withPeerDependencies,
+    });
+  } else {
+    edges = createPackageGraph(allPackages, {
+      namePatterns: packages,
+      includeDependencies: true,
+      includeDependents: true,
+      withDevDependencies: true,
+      withPeerDependencies: true,
+    });
+  }
+  
   fs.writeFileSync("graph-output.js", JSON.stringify(edges));
 }
