f9b88a465b3c2ad2d71c40e15a0f8e611a9ba555
Ken Chau <ken@gizzar.com>
2022-07-24 10:06:39 -0700

    Do linting!

---
+++ scripts/commands/lint.js
@@ -0,0 +1,25 @@
+const { ESLint } = require("eslint");
+const packagePath = process.cwd();
+
+(async function main() {
+  const optionsPath = `${packagePath}/.eslintrc.js`;
+
+  // 1. Create an instance.
+  const eslint = new ESLint({
+    cwd: packagePath,
+    baseConfig: require('../config/eslintrc.js')
+  });
+
+  // 2. Lint files.
+  const results = await eslint.lintFiles(["src/**/*.ts", "tests/**/*.ts"]);
+
+  // 3. Format the results.
+  const formatter = await eslint.loadFormatter("stylish");
+  const resultText = formatter.format(results);
+
+  // 4. Output it.
+  console.log(resultText);
+})().catch((error) => {
+  process.exitCode = 1;
+  console.error(error);
+});
---
+++ scripts/config/eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/consistent-type-exports": "error",
+    "@typescript-eslint/no-non-null-assertion": "off"
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  root: true,
+};
