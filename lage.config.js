c985f414c35fe9fd12ed927be967b563251943ca
Ken <kchau@microsoft.com>
2022-08-01 09:11:11 -0700

    using lage itself to build itself!

--- lage.config.js
+++ lage.config.js
@@ -4,9 +4,12 @@
 module.exports = {
   pipeline: {
     build: ["^build"],
-    test: [],
+    test: ["@lage-run/monorepo-fixture#build"],
     lint: [],
     start: [],
+
+    // TODO: a temporary hack to allow both of these projects to run build with lage
+    "@lage-run/docs-beta#build": ["@lage/docs#build"]
   },
   npmClient: "yarn",
 };
