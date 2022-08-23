b41a9887cc47bb4cd9c3c8709d5c9ef91d8baecb
Ken <kchau@microsoft.com>
2022-08-23 15:57:54 -0700

    adding cache prune and clear functions

---
+++ packages/cli/src/commands/cache/pruneCache.ts
@@ -0,0 +1,32 @@
+import { Logger } from "@lage-run/logger";
+import { stat } from "fs/promises";
+import fs from "fs";
+import path from "path";
+import { getCacheDir, removeCacheEntry } from "./cacheDir";
+import { getWorkspaces } from "workspace-tools";
+
+const MS_IN_A_DAY = 1000 * 60 * 60 * 24;
+
+export async function pruneCache(pruneDays: number, cwd: string, internalCacheFolder: string, logger: Logger) {
+  const prunePeriod = pruneDays || 30;
+  const now = new Date();
+  const workspaces = getWorkspaces(cwd);
+  for (const workspace of workspaces) {
+    const cachePath = getCacheDir(workspace.path, internalCacheFolder);
+
+    if (fs.existsSync(cachePath)) {
+      const entries = fs.readdirSync(cachePath);
+
+      for (const entry of entries) {
+        const entryPath = path.join(cachePath, entry);
+        const entryStat = await stat(entryPath);
+
+        logger.verbose(`clearing cache for ${workspace.name}`);
+
+        if (now.getTime() - entryStat.mtime.getTime() > prunePeriod * MS_IN_A_DAY) {
+          await removeCacheEntry(entryPath, entryStat);
+        }
+      }
+    }
+  }
+}
