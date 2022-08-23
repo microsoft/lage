da2c8ab526d078e72e1f4648fb3b9e001f234de9
Ken <kchau@microsoft.com>
2022-08-23 13:03:23 -0700

    fix formatting

--- packages/cli/src/commands/run/index.ts
+++ packages/cli/src/commands/run/index.ts
@@ -34,7 +34,10 @@ runCommand
   .option("--skip-local-cache", "skips caching locally")
 
   .option("--profile [profile]", "writes a run profile into a file that can be processed by Chromium devtool")
-  .option("--nodearg <nodeArg>", "arguments to be passed to node (e.g. --nodearg=\"--max_old_space_size=1234 --heap-prof\" - set via \"NODE_OPTIONS\" environment variable")
+  .option(
+    "--nodearg <nodeArg>",
+    'arguments to be passed to node (e.g. --nodearg="--max_old_space_size=1234 --heap-prof" - set via "NODE_OPTIONS" environment variable'
+  )
   .option("--continue", "continues the run even on error")
 
   .allowUnknownOption(true)
