b5d98330b600018c24b147a955abdb00feb80ba8
Ken Chau <ken@gizzar.com>
2022-09-18 17:05:27 -0700

    updates website with the tool links actually working!

--- docs-beta/src/data/ToolList.ts
+++ docs-beta/src/data/ToolList.ts
@@ -3,20 +3,24 @@ export const ToolList = [
     title: "Just ___",
     Svg: require("../../static/img/just.svg").default,
     description: "The task library that just works. Use to build, test, and lint your frontend projects.",
+    link: "https://github.com/microsoft/just"
   },
   {
     title: "Beachball",
     Svg: require("../../static/img/beachball1.svg").default,
     description: "The sunniest semantic version bumper. Makes automating npm publishing a breeze.",
+    link: "https://github.com/microsoft/beachball"
   },
   {
     title: "Backfill",
     Svg: require("../../static/img/backfill.svg").default,
     description: "A JavaScript caching library for reducing build time.",
+    link: "https://github.com/microsoft/backfill"
   },
   {
     title: "p-graph",
     Svg: require("../../static/img/p-chart.svg").default,
     description: "Run a promise graph with concurrency control.",
+    link: "https://github.com/microsoft/p-graph"
   },
 ];
