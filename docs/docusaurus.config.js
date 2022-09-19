b90663ba99a5bce7bb45248e4f112f7cc65f47df
Ken Chau <ken@gizzar.com>
2022-09-18 20:35:19 -0700

    moving the beta docs to be real docs!

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,38 +1,44 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Lage',
-  tagline: 'A Beautiful JS Monorepo Task Runner',
-  url: 'https://microsoft.github.io',
-  baseUrl: '/lage/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/lage-logo.svg',
-  organizationName: 'microsoft', // Usually your GitHub org/user name.
-  projectName: 'lage', // Usually your repo name.
-  customFields:{
-    image: 'img/lage-logo.svg'
+  title: "Lage",
+  tagline: "A Beautiful JS Monorepo Task Runner",
+  url: process.env.DEPLOY_URL ? process.env.DEPLOY_URL : "https://microsoft.github.io",
+  baseUrl: "/lage/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/lage-logo.svg",
+  organizationName: "microsoft", // Usually your GitHub org/user name.
+  projectName: "lage", // Usually your repo name.
+  customFields: {
+    image: "img/lage.png",
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: 'https://github.com/microsoft/lage/',
+          editUrl: "https://github.com/microsoft/lage/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: [require.resolve("./src/css/custom.css")],
         },
-      }),
+      },
+    ],
+    [
+      "docusaurus-preset-shiki-twoslash",
+      {
+        themes: ["min-light", "nord"],
+      },
     ],
   ],
 
@@ -40,48 +46,47 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Lage',
         logo: {
-          alt: 'Lage Logo',
-          src: 'img/lage-logo.svg',
+          alt: "Lage Logo",
+          src: "img/lage.png",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'Introducing Lage/Overview',
-            position: 'left',
-            label: 'Guide',
+            type: "doc",
+            docId: "Introduction",
+            position: "left",
+            label: "Guide",
           },
           {
-            href: 'https://github.com/microsoft/lage',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/microsoft/lage",
+            label: "GitHub",
+            position: "right",
           },
         ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Contributing',
-            items: [
-              {
-                label: 'Contributing to Lage',
-                to: '/docs/Contributing/contribution-guide',
-              },
-            ],
-          }
-        ]
       },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
-      colorMode : {
-        defaultMode: 'dark',
-        disableSwitch: true
+      colorMode: {
+        disableSwitch: false,
       },
     }),
+
+  plugins: [
+    require.resolve("@cmfcmf/docusaurus-search-local"),
+    async function tailwindPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+
+          return postcssOptions;
+        },
+      };
+    },
+  ],
 };
 
 module.exports = config;
