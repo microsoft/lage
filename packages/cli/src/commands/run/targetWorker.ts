b171c71668f002fac766ad5e77731ee95cf7737a
Ken Chau <ken@gizzar.com>
2022-09-29 23:27:39 -0700

    get rid of unused imports

--- packages/cli/src/commands/run/targetWorker.ts
+++ packages/cli/src/commands/run/targetWorker.ts
@@ -4,9 +4,7 @@ import { TargetRunnerPicker } from "@lage-run/scheduler";
 import type { TargetRunner } from "@lage-run/scheduler";
 import { findNpmClient } from "@lage-run/find-npm-client";
 import { workerData } from "node:worker_threads";
-import createLogger from "@lage-run/logger";
 import { WorkerRunner } from "@lage-run/scheduler";
-import { initializeReporters } from "@lage-run/reporters";
 import type { ReporterInitOptions } from "@lage-run/reporters";
 
 interface TargetWorkerDataOptions extends ReporterInitOptions {
