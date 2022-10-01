f617f307df40adf606fe6a506bd8f0e971982ebb
Ken Chau <ken@gizzar.com>
2022-10-01 00:12:14 -0700

    added a graph cycle test during run command

--- packages/cli/src/commands/run/action.ts
+++ packages/cli/src/commands/run/action.ts
@@ -1,6 +1,7 @@
 import { BackfillCacheProvider, RemoteFallbackCacheProvider, TargetHasher } from "@lage-run/cache";
 import { Command } from "commander";
 import { createProfileReporter } from "./createProfileReporter";
+import { detectCycles } from "@lage-run/target-graph";
 import { findNpmClient } from "../../workspace/findNpmClient";
 import { getConfig } from "../../config/getConfig";
 import { getFilteredPackages } from "../../filter/getFilteredPackages";
@@ -83,6 +84,12 @@ export async function runAction(options: RunOptions, command: Command) {
   }
 
   const targetGraph = builder.buildTargetGraph(tasks, packages);
+
+  const cycles = detectCycles(targetGraph.targets);
+  if (cycles.hasCycle) {
+    logger.error("Cycles detected in target graph:\n" + cycles.cycle?.join(" -> "));
+    process.exit(1);
+  }
 
   // Create Cache Provider
 
