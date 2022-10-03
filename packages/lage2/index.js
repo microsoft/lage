9b24cb8d95f6e86da0c3ae3ef9246bb007e23b83
Ken <kchau@microsoft.com>
2022-10-03 13:25:15 -0700

    not ready yet for the lage index.js to over write the registerWorker()

--- packages/lage2/index.js
+++ packages/lage2/index.js
@@ -1,9 +1,1 @@
-/** 
- * @deprecated 
- * The use of registerWorker is no longer necessary! Simply use the default export or module.exports from a script file.
- **/
-function registerWorker() {
-  console.warn("registerWorker is deprecated, it is no longer necessary!");
-};
-
-export { registerWorker } 
+export { registerWorker } from "@lage-run/worker-threads-pool";
