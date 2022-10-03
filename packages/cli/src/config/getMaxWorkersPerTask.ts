d80c331c8dd1c09509b39127b8150296a7aeb1bc
Ken Chau <ken@gizzar.com>
2022-10-02 22:48:20 -0700

    make nice with jest 26 and not do "node:" imports

--- packages/cli/src/config/getMaxWorkersPerTask.ts
+++ packages/cli/src/config/getMaxWorkersPerTask.ts
@@ -1,4 +1,4 @@
-import os from "node:os";
+import os from "os";
 import type { ConfigOptions } from "../types/ConfigOptions";
 
 export function getMaxWorkersPerTask(pipelineConfig: ConfigOptions["pipeline"]) {
