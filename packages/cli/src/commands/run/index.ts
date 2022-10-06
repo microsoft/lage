6eb980a65d0c5ca1ff7edc8ab179d9d8096963b0
Pushkar Gupta <pushkargupta@microsoft.com>
2022-10-06 09:31:15 -0700

    formatted

--- packages/cli/src/commands/run/index.ts
+++ packages/cli/src/commands/run/index.ts
@@ -33,7 +33,11 @@ addLoggerOptions(runCommand)
   .option("--reset-cache", "resets the cache, filling it after a run")
   .option("--skip-local-cache", "skips caching locally (defaults to true in CI environments)", isRunningFromCI)
   .option("--profile [profile]", "writes a run profile into a file that can be processed by Chromium devtool")
-  .option("--ignore <ignore...>","ignores files when calculating the scope with `--since` in addition to the files specified in lage.config", [])
+  .option(
+    "--ignore <ignore...>",
+    "ignores files when calculating the scope with `--since` in addition to the files specified in lage.config",
+    []
+  )
   .option(
     "--nodearg|--node-arg <nodeArg>",
     'arguments to be passed to node (e.g. --nodearg="--max_old_space_size=1234 --heap-prof" - set via "NODE_OPTIONS" environment variable'
