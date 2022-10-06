826d186137240462271e00f286f474852118c20f
Ken Chau <ken@gizzar.com>
2022-10-06 00:11:01 -0700

    updated the process env setting in spawn a lage run test in e2e

--- packages/lage/tests/mock/monorepo.ts
+++ packages/lage/tests/mock/monorepo.ts
@@ -169,9 +169,11 @@ export class Monorepo {
   }
 
   run(command: string, args?: string[], silent?: boolean) {
+    const { BACKFILL_CACHE_PROVIDER, BACKFILL_CACHE_PROVIDER_OPTIONS, CI, TF_BUILD, ...env } = process.env;
     return execa.sync("yarn", [...(silent === true ? ["--silent"] : []), command, ...(args || [])], {
       cwd: this.root,
       extendEnv: false,
+      env,
     });
   }
 
