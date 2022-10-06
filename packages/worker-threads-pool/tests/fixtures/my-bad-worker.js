c43ffcd31c9bb80dbf8517af7723d91fe87c938f
Ken Chau <ken@gizzar.com>
2022-10-05 21:20:23 -0700

    fixes unit test to accommodate the new marker

--- packages/worker-threads-pool/tests/fixtures/my-bad-worker.js
+++ packages/worker-threads-pool/tests/fixtures/my-bad-worker.js
@@ -4,10 +4,34 @@ const { parentPort } = require("worker_threads");
 const START_WORKER_STREAM_MARKER = "## WORKER:START:";
 const END_WORKER_STREAM_MARKER = "## WORKER:END:";
 
-parentPort?.on("message", async () => {
-  process.stdout.write(`${START_WORKER_STREAM_MARKER}\n`);
-  process.stderr.write(`${START_WORKER_STREAM_MARKER}\n`);
-  parentPort?.postMessage({ err: "BAD", results: undefined });
-  process.stdout.write(`${END_WORKER_STREAM_MARKER}\n`);
-  process.stderr.write(`${END_WORKER_STREAM_MARKER}\n`);
+
+parentPort?.on("message", async (message) => {
+  let abortController;
+
+  switch (message.type) {
+    case "start":
+      abortController = new AbortController();
+      return message.task && (await start(message.task, abortController.signal, message.id));
+
+    case "abort":
+      return abortController?.abort();
+  }
 });
+
+function fn() {
+  return Promise.reject();
+}
+
+async function start(task, abortSignal, id) {
+  try {
+    process.stdout.write(`${START_WORKER_STREAM_MARKER}${id}\n`);
+    process.stderr.write(`${START_WORKER_STREAM_MARKER}${id}\n`);
+    const results = await fn(task, abortSignal);
+    parentPort?.postMessage({ err: undefined, results });
+  } catch (err) {
+    parentPort?.postMessage({ err, results: undefined });
+  } finally {
+    process.stdout.write(`${END_WORKER_STREAM_MARKER}${id}\n`);
+    process.stderr.write(`${END_WORKER_STREAM_MARKER}${id}\n`);
+  }
+}
