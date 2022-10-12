c98a73b8d7afe5695c8bbf5e1df4dc1a73503fbb
Ken <kchau@microsoft.com>
2022-10-12 13:39:11 -0700

    moving tests

--- packages/format-hrtime/tests/formatDuration.test.ts
+++ packages/format-hrtime/tests/formatDuration.test.ts
@@ -1,6 +1,6 @@
-import { hrtimeDiff } from "../src/formatDuration";
+import { hrtimeDiff, hrToSeconds } from "../src/formatDuration";
 
-describe("formatDuration", () => {
+describe("hrtimeDiff", () => {
   it("should calulate diffs between two hrtime", () => {
     const start: [number, number] = [0, 0];
     const end: [number, number] = [1, 1];
@@ -15,5 +15,27 @@ describe("formatDuration", () => {
     const diff = hrtimeDiff(start, end);
 
     expect(diff).toEqual([0, 999999999]);
+  });
+});
+
+describe("hrToSeconds", () => {
+  it("converts a zero duration in seconds", () => {
+    expect(hrToSeconds([0, 0])).toBe("0.00");
+  });
+
+  it("converts a duration of less than a minute to seconds", () => {
+    expect(hrToSeconds([59, 0])).toBe("59.00");
+  });
+
+  it("converts a duration of over a minute to seconds", () => {
+    expect(hrToSeconds([61, 0])).toBe("61.00");
+  });
+
+  it("converts negative durations as well", () => {
+    expect(hrToSeconds([-59, 0])).toBe("-59.00");
+  });
+
+  it("rounding for nanosecs", () => {
+    expect(hrToSeconds([59, 995000001])).toBe("60.00");
   });
 });
