28b55dfdf0ddf170d7b3d8472a3c409dc25fb510
Ken Chau <ken@gizzar.com>
2022-10-22 19:26:57 -0700

    added unit tests to test the shards in wrappedTarget

--- packages/scheduler/tests/WrappedTarget.test.ts
+++ packages/scheduler/tests/WrappedTarget.test.ts
@@ -23,8 +23,13 @@ function createTarget(packageName: string): Target {
 
 class InProcPool implements Pool {
   constructor(private runner: TargetRunner) {}
-  exec({ target }: { target: Target }, _setup, _teardown, abortSignal?: AbortSignal) {
-    return this.runner.run({ target, abortSignal });
+  exec(
+    { target, shardIndex, shardCount }: { target: Target; shardIndex: number; shardCount: number },
+    _setup,
+    _teardown,
+    abortSignal?: AbortSignal
+  ) {
+    return this.runner.run({ target, abortSignal, shardIndex, shardCount });
   }
   close() {
     return Promise.resolve();
@@ -291,5 +296,141 @@ describe("WrappedTarget", () => {
     await wrappedTarget.run();
 
     expect(wrappedTarget.status).toBe("skipped");
+  });
+
+  it("will run shards with proper shard counts", async () => {
+    const cacheProvider = {
+      async clear() {},
+      async fetch() {
+        return true;
+      },
+      async purge() {},
+      async put() {},
+    } as CacheProvider;
+
+    const hasher = {
+      async hash(target: Target) {
+        return "xyz";
+      },
+    } as TargetHasher;
+
+    const logger = new Logger();
+
+    const runner = {
+      run: jest.fn().mockResolvedValue(true),
+    } as TargetRunner;
+
+    const wrappedTarget = new WrappedTarget({
+      abortController: new AbortController(),
+      cacheProvider,
+      continueOnError: false,
+      hasher,
+      logger,
+      root: process.cwd(),
+      shouldCache: true,
+      shouldResetCache: false,
+      target: { ...createTarget("a"), cache: false, shards: 10 },
+      pool: new InProcPool(runner),
+    });
+
+    expect(wrappedTarget.status).toBe("pending");
+
+    await wrappedTarget.run();
+
+    expect(runner.run).toHaveBeenCalledTimes(10);
+  });
+
+  it("will skip shards if cached", async () => {
+    const cacheProvider = {
+      async clear() {},
+      async fetch() {
+        return true;
+      },
+      async purge() {},
+      async put() {},
+    } as CacheProvider;
+
+    const hasher = {
+      async hash(target: Target) {
+        return "xyz";
+      },
+    } as TargetHasher;
+
+    const logger = new Logger();
+
+    const runner = {
+      run: jest.fn().mockResolvedValue(true),
+    } as TargetRunner;
+
+    const wrappedTarget = new WrappedTarget({
+      abortController: new AbortController(),
+      cacheProvider,
+      continueOnError: false,
+      hasher,
+      logger,
+      root: process.cwd(),
+      shouldCache: true,
+      shouldResetCache: false,
+      target: { ...createTarget("a"), cache: true, shards: 10 },
+      pool: new InProcPool(runner),
+    });
+
+    expect(wrappedTarget.status).toBe("pending");
+
+    await wrappedTarget.run();
+
+    expect(runner.run).toHaveBeenCalledTimes(0);
+  });
+
+  it("will call the runner with the proper shardIndex and shardCount", async () => {
+    const cacheProvider = {
+      async clear() {},
+      async fetch() {
+        return true;
+      },
+      async purge() {},
+      async put() {},
+    } as CacheProvider;
+
+    const hasher = {
+      async hash(target: Target) {
+        return "xyz";
+      },
+    } as TargetHasher;
+
+    const logger = new Logger();
+
+    const runner = {
+      run: jest.fn().mockResolvedValue(true),
+    } as TargetRunner;
+
+    const target = { ...createTarget("a"), cache: false, shards: 3 };
+    const abortController = new AbortController();
+
+    const wrappedTarget = new WrappedTarget({
+      abortController,
+      cacheProvider,
+      continueOnError: false,
+      hasher,
+      logger,
+      root: process.cwd(),
+      shouldCache: true,
+      shouldResetCache: false,
+      target,
+      pool: new InProcPool(runner),
+    });
+
+    expect(wrappedTarget.status).toBe("pending");
+
+    await wrappedTarget.run();
+
+    const mockedRun = (runner.run as jest.Mock).mock;
+
+    expect(mockedRun.calls[0][0].shardIndex).toBe(1);
+    expect(mockedRun.calls[0][0].shardCount).toBe(3);
+    expect(mockedRun.calls[1][0].shardIndex).toBe(2);
+    expect(mockedRun.calls[1][0].shardCount).toBe(3);
+    expect(mockedRun.calls[2][0].shardIndex).toBe(3);
+    expect(mockedRun.calls[2][0].shardCount).toBe(3);
   });
 });
