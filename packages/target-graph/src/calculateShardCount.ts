64e039458f7ad32c3650551e829fa372a15566d7
Ken Chau <ken@gizzar.com>
2022-10-22 21:11:07 -0700

    calculate shard count

--- packages/target-graph/src/calculateShardCount.ts
+++ packages/target-graph/src/calculateShardCount.ts
@@ -1,5 +1,5 @@
-import { Target } from "./types/Target";
-import { TargetConfig } from "./types/TargetConfig";
+import type { Target } from "./types/Target";
+import type { TargetConfig } from "./types/TargetConfig";
 
 export function calculateShardCount(target: Target, shards: TargetConfig["shards"]) {
   if (typeof shards === "undefined") {
