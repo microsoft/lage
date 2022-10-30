eb8ae834410995665a6da88326cbae10f1df71cc
Ken Chau <ken@gizzar.com>
2022-10-30 13:16:52 -0700

    making loadModule work

--- packages/lage2/rollup.config.js
+++ packages/lage2/rollup.config.js
@@ -30,7 +30,7 @@ export default [
         ignoreDynamicRequires: true,
       }),
       json(),
-      terser(),
+      // terser(),
     ],
     external: ["fsevents"],
   },
@@ -73,7 +73,7 @@ export default [
         ignoreDynamicRequires: true,
       }),
       json(),
-      terser(),
+      // terser(),
     ],
   },
 ];
--- packages/scheduler/src/runners/TargetRunnerPicker.ts
+++ packages/scheduler/src/runners/TargetRunnerPicker.ts
@@ -1,4 +1,5 @@
 import path from "path";
+import fs from "fs";
 import { getStartTargetId } from "@lage-run/target-graph";
 import { NoOpRunner } from "./NoOpRunner.js";
 import type { Target } from "@lage-run/target-graph";
@@ -11,7 +12,7 @@ export interface TargetRunnerPickerOptions {
 export class TargetRunnerPicker {
   constructor(private options: TargetRunnerPickerOptions) {}
 
-  pick(target: Target): TargetRunner {
+  async pick(target: Target): Promise<TargetRunner> {
     if (target.id === getStartTargetId()) {
       return NoOpRunner;
     }
@@ -25,7 +26,11 @@ export class TargetRunnerPicker {
       const { script, options } = config;
 
       // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const runnerModule = require(script);
+      const runnerModule = await tryLoadModule(script);
+
+      if (!runnerModule) {
+        throw new Error(`No runner found for target ${target.id}`);
+      }
 
       const base = path.basename(script);
       const runnerName = base.replace(path.extname(base), "");
@@ -41,5 +46,51 @@ export class TargetRunnerPicker {
     }
 
     throw new Error(`No runner found for target ${target.id}`);
+  }
+}
+
+/** read the closest package.json */
+function findPackageJson(script: string) {
+  const dir = path.dirname(script);
+  const packageJsonPath = path.join(dir, "package.json");
+
+  if (fs.existsSync(packageJsonPath)) {
+    return packageJsonPath;
+  } else if (dir === path.dirname(dir)) {
+    return null;
+  } else {
+    return findPackageJson(dir);
+  }
+}
+
+async function tryLoadModule(script: string): Promise<any> {
+  // read the closest package.json
+  const packageJsonPath = findPackageJson(script);
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
+  const moduleType = packageJson.type ?? "script";
+
+  const loadStrategy = {
+    ".js": moduleType === "module" ? ["esm", "cjs"] : ["cjs", "esm"],
+    ".mjs": ["esm"],
+    ".cjs": ["cjs"],
+    "": moduleType === "module" ? ["mjs"] : ["cjs"],
+  };
+
+  for (const [ext, strategies] of Object.entries(loadStrategy)) {
+    let importScript = script;
+
+    if (ext === "") {
+      importScript = script + ".js";
+    }
+
+    if (fs.existsSync(importScript)) {
+      for (const strategy of strategies) {
+        if (strategy === "esm") {
+          return await import(importScript);
+        } else if (strategy === "cjs") {
+          return require(importScript);
+        }
+      }
+    }
   }
 }
