28f10c0fc10f69493806c17d079f372396965c01
Ken <kchau@microsoft.com>
2022-12-06 15:16:03 -0800

    refactored the react components

---
+++ packages/reporters/src/components/ProgressStatus.tsx
@@ -0,0 +1,20 @@
+import { Box, Text } from "ink";
+import * as React from "react";
+import { Progress } from "../types/progressBarTypes";
+
+export interface ProgressStatusProps {
+  progress: Progress
+}
+
+export function ProgressStatus(props: { progress: Progress }) {
+  const { waiting, completed, total } = props.progress;
+  const percentage = total > 0 ? `${((completed / total) * 100).toFixed(2)}%` : "0%";
+
+  const status = `Waiting: ${waiting} | Completed: ${completed} | Total: ${total} | ${percentage}`;
+
+  return (
+    <Box>
+      <Text>{status}</Text>
+    </Box>
+  );
+}
