81e5511c85d07edf41367564b0b7e01d76511709
Kenneth Chau <34725+kenotron@users.noreply.github.com>
2023-01-18 13:29:12 -0800

    Fix missing files in the lage2 bundle... (#540)
    * fixing missing files in the bundle
    
    * Change files

--- packages/lage2/scripts/prebuild.js
+++ packages/lage2/scripts/prebuild.js
@@ -3,17 +3,22 @@ const path = require("path");
 
 const cwd = process.cwd();
 const schedulerPath = path.dirname(require.resolve("@lage-run/scheduler/package.json"));
+const cliPath = path.dirname(require.resolve("@lage-run/cli/package.json"));
 
-const runners = fs.readdirSync(path.join(schedulerPath, "lib", "runners"));
+const runnerDirs = [path.join(schedulerPath, "lib", "runners"), path.join(cliPath, "lib", "commands", "cache", "runners")];
 
 function prebuild() {
-  fs.mkdirSync(path.join(schedulerPath, "runners"), { recursive: true });
-  for (const runner of runners) {
-    if (runner.endsWith("Runner.js")) {
-      const src = path.join(schedulerPath, "lib", "runners", runner);
-      const dest = path.join(cwd, "dist", "runners", runner);
-      fs.mkdirSync(path.dirname(dest), { recursive: true });
-      fs.copyFileSync(src, dest);
+  // Due to the fact that workers require the runner to be in the same directory, we need to copy the runners to the dist folder
+
+  for (const runnerDir of runnerDirs) {
+    for (const runner of fs.readdirSync(runnerDir)) {
+      // By convention, only copy things that end with "Runner.js"
+      if (runner.endsWith("Runner.js")) {
+        const src = path.join(runnerDir, runner);
+        const dest = path.join(cwd, "dist", "runners", runner);
+        fs.mkdirSync(path.dirname(dest), { recursive: true });
+        fs.copyFileSync(src, dest);
+      }
     }
   }
 }
