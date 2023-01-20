50098cad304faf9eabcd7bb20b9811caf346f622
Ken Chau <kchau@microsoft.com>
2023-01-20 14:57:14 -0800

    skip logger to give a more pure output

--- packages/cli/src/commands/run/infoAction.ts
+++ packages/cli/src/commands/run/infoAction.ts
@@ -2,22 +2,13 @@ import type { Command } from "commander";
 import { createCache } from "./createCacheProvider.js";
 import { createTargetGraph } from "./createTargetGraph.js";
 import { filterArgsForTasks } from "./filterArgsForTasks.js";
-import { findNpmClient } from "@lage-run/find-npm-client";
 import { getConfig } from "../../config/getConfig.js";
-import { getMaxWorkersPerTask, getMaxWorkersPerTaskFromOptions } from "../../config/getMaxWorkersPerTask.js";
 import { getPackageInfos, getWorkspaceRoot } from "workspace-tools";
-import { filterPipelineDefinitions } from "./filterPipelineDefinitions.js";
-import { JsonReporter, LogReporter } from "@lage-run/reporters";
-import { SimpleScheduler } from "@lage-run/scheduler";
-import { watch } from "./watcher.js";
+import { JsonReporter } from "@lage-run/reporters";
 
-import type { Reporter } from "@lage-run/logger";
 import createLogger, { LogLevel } from "@lage-run/logger";
 
 import type { ReporterInitOptions } from "@lage-run/reporters";
-import type { SchedulerRunSummary } from "@lage-run/scheduler-types";
-import type { Target } from "@lage-run/target-graph";
-import { getConcurrency } from "../../config/getConcurrency.js";
 
 interface RunOptions extends ReporterInitOptions {
   dependencies: boolean;
@@ -34,12 +25,8 @@ export async function infoAction(options: RunOptions, command: Command) {
   const cwd = process.cwd();
   const config = await getConfig(cwd);
 
-  // Configure logger
+  // Configure logger (just a dummy one)
   const logger = createLogger();
-  const reporter = new JsonReporter({
-    logLevel: LogLevel.info,
-  });
-  logger.addReporter(reporter);
 
   // Build Target Graph
   const root = getWorkspaceRoot(process.cwd())!;
@@ -73,11 +60,14 @@ export async function infoAction(options: RunOptions, command: Command) {
   });
 
   const { targets } = targetGraph;
+  const info = {};
 
   for (const target of targets.values()) {
-    logger.info("", {
-      target,
+    info[target.id] = {
+      ...target,
       hash: await hasher.hash(target),
-    });
+    };
   }
+
+  process.stdout.write(`${JSON.stringify(info, null, 2)}\n`);
 }
