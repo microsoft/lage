1c417781e73caac67354c7488d87584d566bedeb
Ken Chau <kchau@microsoft.com>
2023-01-27 12:09:31 -0800

    added a test case for when the targets are scoped

--- packages/e2e-tests/src/info.test.ts
+++ packages/e2e-tests/src/info.test.ts
@@ -19,4 +19,22 @@ describe("info command", () => {
 
     repo.cleanup();
   });
+
+  it("scoped info test case", () => {
+    const repo = new Monorepo("scoped-info");
+
+    repo.init();
+    repo.addPackage("a", ["b"]);
+    repo.addPackage("b");
+
+    repo.install();
+
+    const results = repo.run("test", ["--info", "--to", "b"]);
+    const output = results.stdout + results.stderr;
+    const jsonOutput = parseNdJson(output);
+
+    expect(jsonOutput.map((entry) => ({ ...entry, cwd: `/some/path/to/${entry.packageName}` }))).toMatchSnapshot();
+
+    repo.cleanup();
+  });
 });
