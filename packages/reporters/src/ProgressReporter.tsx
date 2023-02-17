d87421b74e7a15dac2bfc7aff55af867c8f853cc
Ken <kchau@microsoft.com>
2023-02-16 17:30:25 -0800

    adding task-reporter instead of using ink

--- packages/reporters/src/ProgressReporter.tsx
+++ packages/reporters/src/ProgressReporter.tsx
@@ -1,9 +1,12 @@
-import { ProgressReporterApp } from "./components/ProgressReporterApp";
-import { render } from "ink";
-import * as React from "react";
 import EventEmitter from "events";
 import type { LogEntry, Reporter } from "@lage-run/logger";
 import type { SchedulerRunSummary } from "@lage-run/scheduler-types";
+
+import fs from "fs";
+import path from "path";
+
+// @ts-ignore
+import type { TaskReporter as TaskReporterType, TaskReporterTask } from "@ms-cloudpack/task-reporter";
 
 export class ProgressReporter implements Reporter {
   startTime: [number, number] = [0, 0];
@@ -11,8 +14,36 @@ export class ProgressReporter implements Reporter {
   logEvent: EventEmitter = new EventEmitter();
   logEntries = new Map<string, LogEntry[]>();
 
-  constructor(options: { concurrency: number } = { concurrency: 0 }) {
-    render(<ProgressReporterApp logEvent={this.logEvent} concurrency={options.concurrency} />);
+  taskReporter: TaskReporterType | undefined;
+  tasks: Map<string, TaskReporterTask> = new Map();
+
+  constructor(private options: { concurrency: number; version: string } = { concurrency: 0, version: "0.0.0" }) {
+    
+  }
+
+  async reporter() {
+    if (!this.taskReporter) {
+      const TaskReporter = (await import("@ms-cloudpack/task-reporter")).TaskReporter;
+      this.taskReporter = new TaskReporter({
+        productName: "lage",
+        version: this.options.version,
+        showCompleted: true,
+        showConsoleDebug: true,
+        showConsoleError: true,
+        showConsoleInfo: true,
+        showConsoleLog: true,
+        showConsoleWarn: true,
+        showErrors: true,
+        showPending: true,
+        showProgress: true,
+        showStarted: true,
+        showSummary: true,
+        showTaskDetails: true,
+        showTaskExtended: true,
+      });
+    }
+
+    return this.taskReporter;
   }
 
   log(entry: LogEntry<any>) {
@@ -34,12 +65,12 @@ export class ProgressReporter implements Reporter {
     }
 
     if (entry.data && entry.data.status) {
-      this.logEvent.emit("status", entry);
+      this.logEvent.emit("status", entry.data);
     }
 
-    if (entry.data && entry.data.progress) {
-      this.logEvent.emit("progress", entry.data.progress);
-    }
+    // if (entry.data && entry.data.progress) {
+    //   this.logEvent.emit("progress", entry.data.progress);
+    // }
   }
 
   summarize(schedulerRunSummary: SchedulerRunSummary) {
