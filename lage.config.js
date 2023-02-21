7cb9fd06289a8ace155cb3c952ec1418a5220506
Elizabeth Craig <elcraig@microsoft.com>
2023-02-21 09:18:28 -0800

    fixes

--- lage.config.js
+++ lage.config.js
@@ -1,18 +1,11 @@
 // @ts-check
 const path = require("path");
-const fastGlob = require("fast-glob");
 
 module.exports = {
   pipeline: {
     build: [],
     test: [],
     lint: [],
-    depcheck: {
-      type: "worker",
-      options: {
-        worker: path.join(__dirname, "scripts/worker/depcheck.js"),
-      },
-    },
   },
   npmClient: "yarn",
   cacheOptions: {
