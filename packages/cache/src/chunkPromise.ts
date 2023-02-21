72593d282f50a5b3bf695c8b2cdb3d2e3e4687e5
Ken <kchau@microsoft.com>
2023-02-21 13:16:42 -0800

    Using the shorter version of the loop as suggested by @dzearing

--- packages/cache/src/chunkPromise.ts
+++ packages/cache/src/chunkPromise.ts
@@ -1,16 +1,7 @@
 type PromiseFn = () => Promise<unknown>;
 
 export async function chunkPromise(promises: (Promise<unknown> | PromiseFn)[], limit = 5) {
-  const chunk: (Promise<unknown> | PromiseFn)[] = [];
-  const lastPromise = promises[promises.length - 1];
-
-  for (const promise of promises) {
-    chunk.push(promise);
-    const chunkCount = chunk.length;
-
-    if (chunkCount % limit === 0 || promise === lastPromise) {
-      await Promise.all(chunk.map((p) => (typeof p === "function" ? p() : p)));
-      chunk.splice(0, chunk.length);
-    }
+  for (let i = 0; i < promises.length; i += limit) {
+    await Promise.all(promises.slice(i, i + limit).map((p) => (typeof p === "function" ? p() : p)));
   }
 }
--- packages/cache/tests/chunkPromise.test.ts
+++ packages/cache/tests/chunkPromise.test.ts
@@ -3,11 +3,11 @@ import { chunkPromise } from "../src/chunkPromise";
 describe("chunking promises", () => {
   it("should chunk promises", async () => {
     const mockedPromiseFns = [
-      jest.fn().mockResolvedValue(1),
-      jest.fn().mockResolvedValue(2),
-      jest.fn().mockResolvedValue(3),
-      jest.fn().mockResolvedValue(4),
-      jest.fn().mockResolvedValue(5),
+      jest.fn().mockResolvedValue(1).mockName("1"),
+      jest.fn().mockResolvedValue(2).mockName("2"),
+      jest.fn().mockResolvedValue(3).mockName("3"),
+      jest.fn().mockResolvedValue(4).mockName("4"),
+      jest.fn().mockResolvedValue(5).mockName("5"),
     ];
 
     await chunkPromise(mockedPromiseFns, 2);
@@ -19,11 +19,11 @@ describe("chunking promises", () => {
 
   it("should throw, if one promise was rejected", async () => {
     const mockedPromiseFns = [
-      jest.fn().mockResolvedValue(1),
-      jest.fn().mockRejectedValue(2),
-      jest.fn().mockResolvedValue(3),
-      jest.fn().mockResolvedValue(4),
-      jest.fn().mockResolvedValue(5),
+      jest.fn().mockResolvedValue(1).mockName("1"),
+      jest.fn().mockResolvedValue(2).mockName("2"),
+      jest.fn().mockResolvedValue(3).mockName("3"),
+      jest.fn().mockResolvedValue(4).mockName("4"),
+      jest.fn().mockResolvedValue(5).mockName("5"),
     ];
 
     expect(async () => await chunkPromise(mockedPromiseFns, 2)).rejects;
