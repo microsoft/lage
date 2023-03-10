5fd1b1eb7f87917f3abec9e99b240d38846eea09
Kenneth Chau <34725+kenotron@users.noreply.github.com>
2023-03-09 17:20:06 -0800

    Adds the ability to create no-op tasks (#597)
    * adding the ability to have a NoOp Task
    
    * Change files
    
    * further fix noop runner launcher
    
    * added some tests for targetfactory
    
    * get rid of lint problems
    
    * info updated
    
    * update snapshots

---
+++ packages/target-graph/tests/TargetFactory.test.ts
@@ -0,0 +1,65 @@
+import path from "path";
+import { TargetFactory } from "../src/TargetFactory";
+
+describe("TargetFactory", () => {
+  it("should give a type of 'npmScript' if one of the packages contain that script", () => {
+    const factory = new TargetFactory({
+      root: "root",
+      packageInfos: {
+        a: {
+          name: "a",
+          packageJsonPath: "root/a/package.json",
+          version: "1.0.0",
+          scripts: {
+            build: "echo build",
+          },
+        },
+      },
+      resolve(packageName) {
+        return path.join("root", packageName);
+      },
+    });
+
+    const target = factory.createPackageTarget("a", "build", {
+      dependsOn: ["^build"],
+    });
+
+    expect(target.type).toBe("npmScript");
+  });
+
+  it("should give a type of 'noop' if one of the packages contain that script", () => {
+    const factory = new TargetFactory({
+      root: "root",
+      packageInfos: {
+        a: {
+          name: "a",
+          packageJsonPath: "root/a/package.json",
+          version: "1.0.0",
+          scripts: {
+            build: "echo build",
+          },
+        },
+        b: {
+          name: "b",
+          packageJsonPath: "root/a/package.json",
+          version: "1.0.0",
+          scripts: {
+            build: "echo build",
+          },
+          dependencies: {
+            a: "1.0.0",
+          },
+        },
+      },
+      resolve(packageName) {
+        return path.join("root", packageName);
+      },
+    });
+
+    const target = factory.createPackageTarget("a", "unknown", {
+      dependsOn: ["^build"],
+    });
+
+    expect(target.type).toBe("noop");
+  });
+});
