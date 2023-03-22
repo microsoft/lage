fad4d8a6cb9d1204f6b83f6ab2f7f0205529df2e
Elizabeth Craig <elcraig@microsoft.com>
2023-03-22 10:23:02 -0700

    Add bundled config types to lage package (#606)

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,7 @@
+export type { CacheOptions } from "./types/CacheOptions.js";
 export type { ConfigOptions } from "./types/ConfigOptions.js";
+export type { Priority } from "./types/Priority.js";
+export type { PipelineDefinition } from "./types/PipelineDefinition.js";
+export type { LoggerOptions } from "./types/LoggerOptions.js";
+export type { TargetRunnerPickerOptions } from "@lage-run/scheduler";
+export type { TargetConfig } from "@lage-run/target-graph";
