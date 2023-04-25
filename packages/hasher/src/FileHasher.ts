534ccfe350e98bc9d3c2dcd98f74e31864b86782
Ken <kchau@microsoft.com>
2023-04-25 16:34:44 -0700

    updated to use glob-hasher's stat fn

--- packages/hasher/src/FileHasher.ts
+++ packages/hasher/src/FileHasher.ts
@@ -1,8 +1,9 @@
-import fsp from "fs/promises";
 import fs from "fs";
 import path from "path";
-import { hash as fastHash } from "glob-hasher";
+import { hash as fastHash, stat } from "glob-hasher";
 import { createInterface } from "node:readline";
+
+import fg from "fast-glob";
 
 interface FileHashStoreOptions {
   root: string;
@@ -12,11 +13,8 @@ export class FileHasher {
   #store: Record<string, string> = {};
   #manifestFile: string;
 
-  async getKey(file: string) {
-    const { root } = this.options;
-    const relativePath = path.relative(root, file);
-    const { mtimeMs, size } = await fsp.stat(file);
-    return `${mtimeMs}-${size}-${relativePath}`;
+  getKey(relativePath: string, mtime: BigInt, size: number) {
+    return `${mtime}-${size}-${relativePath}`;
   }
 
   constructor(private options: FileHashStoreOptions) {
@@ -26,43 +24,63 @@ export class FileHasher {
   }
 
   async readManifest() {
-    const inputStream = await fs.createReadStream(this.#manifestFile, "utf-8");
-    const rl = createInterface(inputStream);
-    rl.on("line", (line) => {
-      const [key, hash] = line.split(":");
-      this.#store[key] = hash;
+    return new Promise<void>((resolve, reject) => {
+      const inputStream = fs.createReadStream(this.#manifestFile, "utf-8");
+      const rl = createInterface({
+        input: inputStream,
+        crlfDelay: Infinity,
+      });
+
+      rl.on("line", (line) => {
+        const [key, hash] = line.split(":");
+        this.#store[key] = hash;
+      });
+
+      inputStream.on("end", () => {
+        rl.close();
+        resolve();
+      });
     });
-    inputStream.close();
   }
 
-  async writeManifest() {
-    const outputStream = await fs.createWriteStream(this.#manifestFile, "utf-8");
+  writeManifest() {
+    const outputStream = fs.createWriteStream(this.#manifestFile, "utf-8");
     for (const [key, hash] of Object.entries(this.#store)) {
       outputStream.write(`${key}:${hash}\n`);
     }
-    outputStream.close();
+
+    outputStream.end();
   }
+
+  async initializeManifest() {}
 
   async hash(files: string[]) {
     const hashes: Record<string, string> = {};
 
     const updatedFiles: string[] = [];
 
-    for (const file of files) {
-      const key = await this.getKey(file);
-      const hash = await this.#store[key];
-      if (hash) {
-        hashes[file] = hash;
-      } else {
-        updatedFiles.push(file);
-      }
-    }
+    const stats = stat(files, { cwd: this.options.root }) ?? {};
+    await Promise.all(
+      files.map(async (file) => {
+        const stat = stats[file];
+        const key = this.getKey(file, stat?.mtime, stat?.size);
+        const hash = this.#store[key];
+        if (hash) {
+          hashes[file] = hash;
+        } else {
+          updatedFiles.push(file);
+        }
+      })
+    );
 
+    console.time("hash");
     const updatedHashes = fastHash(updatedFiles, { cwd: this.options.root }) ?? {};
+    console.timeEnd("hash");
 
-    Promise.all(
+    await Promise.all(
       Object.entries(updatedHashes).map(async ([file, hash]) => {
-        const key = await this.getKey(file);
+        const stat = fs.statSync(path.join(this.options.root, file), { bigint: true });
+        const key = this.getKey(file, stat.mtimeMs, Number(stat.size));
         this.#store[key] = hash;
         hashes[file] = hash;
       })
@@ -70,4 +88,31 @@ export class FileHasher {
 
     return hashes;
   }
+}
+
+if (require.main === module) {
+  (async () => {
+    const root = "/workspace/tmp1";
+    const hasher = new FileHasher({ root });
+
+    console.time("fg");
+    const files = await fg(["**/*"], { cwd: root, ignore: ["**/node_modules"] });
+    console.timeEnd("fg");
+
+    console.log(files.length);
+
+    console.time("read");
+    await hasher.readManifest();
+    console.timeEnd("read");
+
+    console.time("hasher.hash");
+    const hashes = await hasher.hash(files);
+    console.timeEnd("hasher.hash");
+
+    console.time("write");
+    await hasher.writeManifest();
+    console.timeEnd("write");
+
+    console.log(hashes);
+  })();
 }
