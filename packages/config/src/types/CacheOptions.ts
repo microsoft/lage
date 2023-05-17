be520b9be2b725db9a5db33a1b2766a5e94811da
Altinok Darici <altinokd@microsoft.com>
2023-05-17 09:49:25 -0700

    Remove custom storage config from cache options

--- packages/config/src/types/CacheOptions.ts
+++ packages/config/src/types/CacheOptions.ts
@@ -1,6 +1,10 @@
-import type { Config as BackfillCacheOptions } from "backfill-config";
+import type { Config as BackfillCacheOptions, CustomStorageConfig } from "backfill-config";
 
-export type CacheOptions = BackfillCacheOptions & {
+type LageBackfillCacheOptions = Omit<BackfillCacheOptions, "cacheStorageConfig"> & {
+  cacheStorageConfig: Exclude<BackfillCacheOptions["cacheStorageConfig"], CustomStorageConfig>;
+};
+
+export type CacheOptions = LageBackfillCacheOptions & {
   environmentGlob: string[];
   cacheKey: string;
   writeRemoteCache: boolean;
