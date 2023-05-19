429b1e61a7237a95e1d8f3c636b51db7de748fcf
Kenneth Chau <34725+kenotron@users.noreply.github.com>
2023-05-19 14:42:26 -0700

    Worker.on("error") handling (OOM, non memory leak case) (#668)
    * crash immediate on OOM in worker (not the memory leak case)
    
    * Change files

--- packages/worker-threads-pool/src/ThreadWorker.ts
+++ packages/worker-threads-pool/src/ThreadWorker.ts
@@ -96,16 +96,18 @@ export class ThreadWorker extends EventEmitter implements IWorker {
     worker.on("message", msgHandler);
 
     const errHandler = (err) => {
-      Promise.all([this.#stdoutInfo.promise, this.#stderrInfo.promise]).then(() => {
-        // In case of an uncaught exception: Call the callback that was passed to
-        // `runTask` with the error.
-        if (this.#taskInfo) {
-          this.#taskInfo.done(err, null);
-        }
+      // We likely have a worker that has crashed - many instances of this is due to out-of-memory errors, we need to fail fast!
+      this.#stdoutInfo.resolve();
+      this.#stderrInfo.resolve();
 
+      // In case of an uncaught exception: Call the callback that was passed to
+      // `runTask` with the error, otherwise, just emit an "error" event (which will crash the process if not handled)
+      if (this.#taskInfo) {
+        this.#taskInfo.abortSignal?.removeEventListener("abort", this.#handleAbort);
+        this.#taskInfo.done(err, null);
+      } else {
         this.emit("error", err);
-        this.restart();
-      });
+      }
     };
 
     // The 'error' event is emitted if the worker thread throws an uncaught exception. In that case, the worker is terminated.
@@ -190,7 +192,9 @@ export class ThreadWorker extends EventEmitter implements IWorker {
   }
 
   #handleAbort() {
-    this.#worker.postMessage({ type: "abort" });
+    if (this.#worker) {
+      this.#worker.postMessage({ type: "abort" });
+    }
   }
 
   start(work: QueueItem, abortSignal?: AbortSignal) {
