9d51f4d871a6c9b11d462f29ee923a73f5fda663
Altinok Darici <altinokd@microsoft.com>
2023-05-25 08:35:45 -0700

    Fix tests

--- packages/e2e-tests/src/remoteFallback.test.ts
+++ packages/e2e-tests/src/remoteFallback.test.ts
@@ -2,6 +2,54 @@ import { Monorepo } from "./mock/monorepo.js";
 import { parseNdJson } from "./parseNdJson.js";
 
 describe("RemoteFallbackCacheProvider", () => {
+  it("should skip local cache population if --skip-local-cache is enabled", () => {
+    const repo = new Monorepo("fallback");
+
+    repo.init();
+    repo.setLageConfig(
+      `const fs = require('fs');
+      const path = require('path');
+      module.exports = {
+        pipeline: {
+          build: [],
+          test: []
+        },
+        cache: true,
+        cacheOptions: {
+          writeRemoteCache: true,
+          cacheStorageConfig: {
+            provider: 'local'
+          },
+          internalCacheFolder: '.lage-cache-test'
+        } 
+      };`
+    );
+
+    repo.addPackage("a", [], {
+      build: "echo a:build",
+      test: "echo a:test",
+    });
+    repo.addPackage("b", [], {
+      build: "echo b:build",
+    });
+    repo.install();
+
+    const results = repo.run("test", ["--skip-local-cache"]);
+
+    const output = results.stdout + results.stderr;
+    const jsonOutput = parseNdJson(output);
+
+    expect(jsonOutput.find((entry) => entry.msg?.includes("local cache fetch"))).toBeFalsy();
+
+    expect(jsonOutput.find((entry) => entry.msg?.includes("remote fallback fetch"))).toBeTruthy();
+
+    expect(jsonOutput.find((entry) => entry.msg?.includes("local cache put"))).toBeFalsy();
+
+    expect(jsonOutput.find((entry) => entry.msg?.includes("remote fallback put"))).toBeTruthy();
+
+    repo.cleanup();
+  });
+
   it("should operate with local provider ONLY by default", () => {
     const repo = new Monorepo("fallback-local-only");
 
@@ -42,6 +90,101 @@ describe("RemoteFallbackCacheProvider", () => {
     expect(jsonOutput.find((entry) => entry.msg?.includes("local cache put"))).toBeTruthy();
 
     expect(jsonOutput.find((entry) => entry.msg?.includes("remote fallback put"))).toBeFalsy();
+
+    repo.cleanup();
+  });
+
+  it("should allow read-only mode when given a remote (or custom) cache config", () => {
+    const repo = new Monorepo("fallback-read-only");
+
+    repo.init();
+    repo.setLageConfig(
+      `const fs = require('fs');
+      const path = require('path');
+      module.exports = {
+        pipeline: {
+          build: [],
+          test: []
+        },
+        cache: true,
+        cacheOptions: {
+          cacheStorageConfig: {
+            provider: 'local'
+          },
+          internalCacheFolder: '.lage-cache-test'
+        }  
+      };`
+    );
+
+    repo.addPackage("a", [], {
+      build: "echo a:build",
+      test: "echo a:test",
+    });
+    repo.addPackage("b", [], {
+      build: "echo b:build",
+    });
+    repo.install();
+
+    const results = repo.run("test", ["--log-level", "silly"]);
+
+    const output = results.stdout + results.stderr;
+    const jsonOutput = parseNdJson(output);
+
+    expect(jsonOutput.find((entry) => entry.msg?.includes("local cache fetch"))).toBeTruthy();
+
+    expect(jsonOutput.find((entry) => entry.msg?.includes("remote fallback fetch"))).toBeTruthy();
+
+    expect(jsonOutput.find((entry) => entry.msg?.includes("local cache put"))).toBeTruthy();
+
+    expect(jsonOutput.find((entry) => entry.msg?.includes("remote fallback put"))).toBeFalsy();
+
+    repo.cleanup();
+  });
+
+  it("should allow read-write mode when given a special environment variable", () => {
+    const repo = new Monorepo("fallback-read-write-env-var");
+
+    repo.init();
+    repo.setLageConfig(
+      `const fs = require('fs');
+      const path = require('path');
+      module.exports = {
+        pipeline: {
+          build: [],
+          test: []
+        },
+        cache: true,
+        cacheOptions: {
+          writeRemoteCache: true,
+          cacheStorageConfig: {
+            provider: 'local'
+          },
+          internalCacheFolder: '.lage-cache-test'
+        }
+      };`
+    );
+
+    repo.addPackage("a", [], {
+      build: "echo a:build",
+      test: "echo a:test",
+    });
+    repo.addPackage("b", [], {
+      build: "echo b:build",
+    });
+    repo.install();
+
+    const results = repo.run("test", ["--log-level", "silly"]);
+
+    const output = results.stdout + results.stderr;
+    const jsonOutput = parseNdJson(output);
+
+    expect(jsonOutput.find((entry) => entry.msg?.includes("local cache fetch"))).toBeTruthy();
+
+    expect(jsonOutput.find((entry) => entry.msg?.includes("remote fallback fetch"))).toBeTruthy();
+
+    expect(jsonOutput.find((entry) => entry.msg?.includes("local cache put"))).toBeTruthy();
+
+    expect(jsonOutput.find((entry) => entry.msg?.includes("remote fallback put"))).toBeTruthy();
 
     repo.cleanup();
   });
