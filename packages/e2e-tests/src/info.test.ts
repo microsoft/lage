9346c9d5b06973485c23cd3269228f91b0d79cbb
Sverre Johansen <sverre.johansen@gmail.com>
2024-02-26 17:16:19 +0100

    Fix lage info dependencies by resolving transitive task dependencies behind noop tasks (#723)
    * Resolve transitive task dependencies behind noop tasks
    
    * Add e2e test for resolving dependencies via noop tasks.
    
    * Change files

--- packages/e2e-tests/src/info.test.ts
+++ packages/e2e-tests/src/info.test.ts
@@ -37,4 +37,34 @@ describe("info command", () => {
 
     repo.cleanup();
   });
+
+  it("dependencies are resolved via noop tasks", () => {
+    const repo = new Monorepo("noop-task-info");
+    repo.init();
+    repo.addPackage("a", ["b"], { build: "echo 'building a'" });
+    // This task does not have a `build` script.
+    repo.addPackage("b", ["c"], {});
+    repo.addPackage("c", [], { build: "echo 'building c'" });
+    repo.install();
+
+    const results = repo.run("lage", ["info", "build", "prepare"]);
+
+    const output = results.stdout + results.stderr;
+    const infoJsonOutput: any = parseNdJson(output)[0];
+    const { packageTasks } = infoJsonOutput.data;
+
+    // Check if task `a#build` depends on `c#build`, because package `b` doesn't
+    // have a `build` task so dependencies are hoisted up.
+    const task = packageTasks.find(({ id }) => id === "a#build");
+    expect(task.dependencies).toEqual(["c#build"]);
+
+    // Make sure all dependencies points to an existing task.
+    for (const task of packageTasks) {
+      for (const dependency of task.dependencies) {
+        expect(packageTasks.some(({ id }) => id === dependency)).toBeTruthy();
+      }
+    }
+
+    repo.cleanup();
+  });
 });
