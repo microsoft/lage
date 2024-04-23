9b056cf6190b9a26d71f2289df2d4dae73bb8f47
Elizabeth Craig <elcraig@microsoft.com>
2024-04-23 16:50:55 -0700

    fix lage config globs

--- lage.config.js
+++ lage.config.js
@@ -58,14 +58,21 @@ module.exports = {
   },
   npmClient: "yarn",
   cacheOptions: {
+    // These are relative to the git root, and affect the hash of the cache.
+    // Changes to any of these files will invalidate the cache.
     environmentGlob: [
+      // Folder globs MUST end with **/* to include all files!
+      "!node_modules/**/*",
+      "!**/node_modules/**/*",
+      ".github/workflows/*",
       "beachball.config.js",
       "lage.config.js",
       "package.json",
-      "renovate.json5",
-      ".github/**",
       "packages/tsconfig.lage2.json",
-      "patches",
+      "patches/**/*",
+      "yarn.lock",
     ],
+    // Subset of files in package directories that will be saved into the cache.
+    outputGlob: ["lib/**/*", "dist/**/*", ".docusaurus/**/*", "build/**/*"],
   },
 };
