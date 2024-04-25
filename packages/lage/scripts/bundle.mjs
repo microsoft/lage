540dfc56c299441ea449427f210f7116ae03b806
Elizabeth Craig <elcraig@microsoft.com>
2024-04-24 18:25:44 -0700

    bundle

--- packages/lage/scripts/bundle.mjs
+++ packages/lage/scripts/bundle.mjs
@@ -3,11 +3,15 @@ import * as esbuild from "esbuild";
 import fs from "fs";
 import { createRequire } from "module";
 import path from "path";
+import { findPackageRoot } from "workspace-tools";
 
 const localRequire = createRequire(import.meta.url);
 
 async function bundle() {
   console.log("\nBundling lage with esbuild...");
+
+  const packageRoot = findPackageRoot(process.cwd()) || process.cwd();
+  const packageJson = JSON.parse(fs.readFileSync(path.join(packageRoot, "package.json"), "utf-8"));
 
   // Mapping from output path (relative to dist, no extension) to input path (relative to package root)
   const entryPoints = {
@@ -16,7 +20,13 @@ async function bundle() {
     "workers/targetWorker": "@lage-run/scheduler/lib/workers/targetWorker.js",
   };
   // List of external modules that should not be bundled
-  const external = ["fsevents", "glob-hasher", "./workers/targetWorker"];
+  const external = [
+    // Externalize deps and optional deps, which are native packages
+    ...Object.keys(packageJson.dependencies),
+    ...Object.keys(packageJson.optionalDependencies),
+    // Also don't re-bundle targetWorker from the other entry points
+    "./workers/targetWorker",
+  ];
 
   // Due to the fact that workers require the runner to be in the same directory,
   // add the runners to the entry points, as well as the externals (so the file is preserved).
@@ -37,26 +47,31 @@ async function bundle() {
     }
   }
 
-  await esbuild.build({
+  /** @type {import('esbuild').BuildOptions} */
+  const esbuildOptions = {
+    absWorkingDir: packageRoot,
     entryPoints,
     bundle: true,
     platform: "node",
     target: ["node16"],
-    outdir: "dist",
-    sourcemap: true,
     logLevel: "info",
     external,
+  };
+
+  // Minified build
+  console.log("\nCreating minified bundles under dist");
+  await esbuild.build({
+    ...esbuildOptions,
+    outdir: "dist",
+    sourcemap: true,
     minify: true,
   });
 
+  // Debug build
+  console.log("\nCreating debug bundles under dist/debug");
   await esbuild.build({
-    entryPoints,
-    bundle: true,
-    platform: "node",
-    target: ["node16"],
+    ...esbuildOptions,
     outdir: "dist/debug",
-    logLevel: "info",
-    external,
     minify: false,
   });
 }
