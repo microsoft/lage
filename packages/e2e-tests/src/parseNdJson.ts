a51dec3594f3189c50c147d757055938a895f624
Ken Chau <kchau@microsoft.com>
2024-09-11 13:19:10 -0700

    filter out the line that says "Done ..."

--- packages/e2e-tests/src/parseNdJson.ts
+++ packages/e2e-tests/src/parseNdJson.ts
@@ -2,14 +2,16 @@ import type { TargetStatus } from "@lage-run/scheduler-types";
 
 export function parseNdJson(ndjson: string) {
   const entries = ndjson.substr(ndjson.indexOf("{"));
-
-  return entries.split("\n").map((line) => {
-    try {
-      return JSON.parse(line);
-    } catch (e) {
-      return {};
-    }
-  });
+  return entries
+    .split("\n")
+    .map((line) => {
+      try {
+        return JSON.parse(line);
+      } catch (e) {
+        return {};
+      }
+    })
+    .filter((entry) => Object.keys(entry).length > 0);
 }
 
 export function filterEntry(data: any, pkg: string, task: string, status: TargetStatus) {
