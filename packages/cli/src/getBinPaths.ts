8a97705e2ed833a5ecb64e228357ded2f5cd9828
Ken Chau <kchau@microsoft.com>
2024-10-08 11:33:10 -0700

    changing the launch to use the bin script

--- packages/cli/src/getBinPaths.ts
+++ packages/cli/src/getBinPaths.ts
@@ -1,16 +1,26 @@
 import path from "path";
 import fs from "fs";
+import os from "os";
+
+function findUp(name: string, dir: string) {
+  let currentDir = dir;
+  while (currentDir !== "/") {
+    const fullPath = path.join(currentDir, name);
+    if (fs.existsSync(fullPath)) {
+      return fullPath;
+    }
+    currentDir = path.dirname(currentDir);
+  }
+  return undefined;
+}
 
 export function getBinPaths() {
-  let dir = __dirname;
-  let packageJsonPath = "";
-  while (dir !== "/") {
-    packageJsonPath = path.join(dir, "package.json");
-    if (fs.existsSync(packageJsonPath)) {
-      break;
-    }
-    dir = path.dirname(dir);
+  const bins = os.platform() === "win32" ? ["lage.cmd", "lage-server.cmd"] : ["lage", "lage-server"];
+  const binPaths = bins.map((bin) => findUp("node_modules/.bin/" + bin, __dirname));
+
+  if (binPaths.some((binPath) => binPath === undefined)) {
+    throw new Error("Could not find bin paths for lage or lage-server");
   }
-  const packageJson = JSON.parse(fs.readFileSync(path.join(dir, "package.json"), "utf8"));
-  return { lage: path.join(dir, packageJson.bin.lage), "lage-server": path.join(dir, packageJson.bin["lage-server"]) };
+
+  return { lage: binPaths[0]!, "lage-server": binPaths[1]! };
 }
