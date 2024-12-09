14461e2d09441e3b775c98b00f840874f68ccdac
Ken Chau <kchau@microsoft.com>
2024-12-09 04:38:52 +0000

    moving to vanilla js node.js

--- packages/rpc/src/createServer.ts
+++ packages/rpc/src/createServer.ts
@@ -1,23 +1,12 @@
-import { fastify } from "fastify";
-import { fastifyConnectPlugin } from "@connectrpc/connect-fastify";
 import { createRoutes } from "./createRoutes.js";
+import { connectNodeAdapter } from "@connectrpc/connect-node";
+import http2 from "http2";
 import type { ILageService } from "./types/ILageService.js";
 
 export async function createServer(lageService: ILageService, abortController: AbortController) {
-  const server = fastify({
-    http2: true,
-  });
-  await server.register(fastifyConnectPlugin, {
-    routes: createRoutes(lageService),
-    shutdownSignal: abortController.signal,
-    compressMinBytes: 512,
-    grpc: true,
-  });
-
-  server.get("/", (_, reply) => {
-    reply.type("text/plain");
-    reply.send("lage service");
-  });
+  const server = http2.createServer(
+    connectNodeAdapter({ routes: createRoutes(lageService), grpc: true, shutdownSignal: abortController.signal }) // responds with 404 for other requests
+  );
 
   return server;
 }
