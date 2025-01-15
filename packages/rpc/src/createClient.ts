59ee2e6dcff9e9ca072895a7f12be260e1f09a34
Ken Chau <kchau@microsoft.com>
2025-01-15 21:00:02 +0000

    updates to rpc

--- packages/rpc/src/createClient.ts
+++ packages/rpc/src/createClient.ts
@@ -1,8 +1,6 @@
-import { createPromiseClient, PromiseClient } from "@connectrpc/connect";
+import { createPromiseClient, type PromiseClient } from "@connectrpc/connect";
 import { createGrpcTransport } from "@connectrpc/connect-node";
 import { LageService } from "./gen/lage/v1/lage_connect.js";
-import { MethodKind } from "@bufbuild/protobuf";
-import { RunTargetRequest, RunTargetResponse, PingRequest, PingResponse } from "./gen/lage/v1/lage_pb.js";
 
 export interface CreateClientOptions {
   baseUrl: string;
@@ -11,23 +9,7 @@ export interface CreateClientOptions {
 
 export type LageClient = ReturnType<typeof createClient>;
 
-export function createClient({ baseUrl, httpVersion }: CreateClientOptions): PromiseClient<{
-    readonly typeName: "connectrpc.lage.v1.LageService";
-    readonly methods: {
-        readonly runTarget: {
-            readonly name: "RunTarget";
-            readonly I: RunTargetRequest;
-            readonly O: RunTargetResponse;
-            readonly kind: MethodKind.Unary;
-        };
-        readonly ping: {
-            readonly name: "Ping";
-            readonly I: PingRequest;
-            readonly O: PingResponse;
-            readonly kind: MethodKind.Unary;
-        };
-    };
-}> {
+export function createClient({ baseUrl, httpVersion }: CreateClientOptions): PromiseClient<typeof LageService> {
   const transport = createGrpcTransport({
     httpVersion,
     baseUrl,
--- packages/rpc/src/createServer.ts
+++ packages/rpc/src/createServer.ts
@@ -3,10 +3,17 @@ import { fastifyConnectPlugin } from "@connectrpc/connect-fastify";
 import { createRoutes } from "./createRoutes.js";
 import type { ILageService } from "./types/ILageService.js";
 
-export async function createServer(lageService: ILageService, abortController: AbortController): Promise<import("fastify").FastifyInstance<import("http2").Http2Server, import("http2").Http2ServerRequest, import("http2").Http2ServerResponse, import("fastify").FastifyBaseLogger, import("fastify").FastifyTypeProviderDefault>> {
+import type { FastifyInstance, FastifyBaseLogger, FastifyTypeProviderDefault } from "fastify";
+import type { Http2Server, Http2ServerRequest, Http2ServerResponse } from "http2";
+
+export async function createServer(
+  lageService: ILageService,
+  abortController: AbortController
+): Promise<FastifyInstance<Http2Server, Http2ServerRequest, Http2ServerResponse, FastifyBaseLogger, FastifyTypeProviderDefault>> {
   const server = fastify({
     http2: true,
   });
+
   await server.register(fastifyConnectPlugin, {
     routes: createRoutes(lageService),
     shutdownSignal: abortController.signal,
