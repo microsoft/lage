6762aeb5d7fe949f9cc35e8d330359b838aab632
Ken Chau <kchau@microsoft.com>
2025-01-17 20:56:25 +0000

    adding test cases

---
+++ packages/hasher/src/__tests__/PackageTree.test.ts
@@ -0,0 +1,78 @@
+import path from "path";
+
+import { PackageTree, TargetHasher } from "../index";
+import fs from "fs";
+import { Monorepo } from "@lage-run/monorepo-fixture";
+import { Target } from "@lage-run/target-graph";
+import { getPackageInfos } from "workspace-tools";
+const fixturesPath = path.join(__dirname, "..", "__fixtures__");
+
+describe("PackageTree", () => {
+  test("can find all files of a monorepo", async () => {
+    const monorepo = new Monorepo("monorepo");
+    await monorepo.init(path.join(fixturesPath, "monorepo"));
+    const packageRoot = monorepo.root;
+
+    const packageInfos = getPackageInfos(packageRoot);
+
+    const packageTree = new PackageTree({
+      includeUntracked: true,
+      root: packageRoot,
+      packageInfos,
+    });
+
+    await packageTree.initialize();
+
+    const files = await packageTree.getPackageFiles("package-a", ["**/*"]);
+
+    expect(files).toHaveLength(3);
+  });
+
+  test("can find all files of a monorepo with a pattern", async () => {
+    const monorepo = new Monorepo("monorepo2");
+    await monorepo.init(path.join(fixturesPath, "monorepo"));
+    const packageRoot = monorepo.root;
+
+    const packageInfos = getPackageInfos(packageRoot);
+
+    const packageTree = new PackageTree({
+      includeUntracked: true,
+      root: packageRoot,
+      packageInfos,
+    });
+
+    await packageTree.initialize();
+
+    const files = await packageTree.getPackageFiles("package-a", ["**/*.ts"]);
+
+    expect(files).toHaveLength(1);
+  });
+
+  test("can find all files of a monorepo with nested files", async () => {
+    const monorepo = new Monorepo("monorepo-nested");
+    await monorepo.init(path.join(fixturesPath, "monorepo-nested"));
+    const packageRoot = monorepo.root;
+
+    const packageInfos = getPackageInfos(packageRoot);
+
+    const packageTree = new PackageTree({
+      includeUntracked: true,
+      root: packageRoot,
+      packageInfos,
+    });
+
+    await packageTree.initialize();
+
+    const files = await packageTree.getPackageFiles("package-a", ["**/*.ts"]);
+    const allFiles = await packageTree.getPackageFiles("package-a", ["**/*"]);
+
+    expect(files).toHaveLength(1);
+    expect(allFiles).toHaveLength(3);
+
+    const files2 = await packageTree.getPackageFiles("package-b", ["**/*.ts"]);
+    const allFiles2 = await packageTree.getPackageFiles("package-b", ["**/*"]);
+
+    expect(files2).toHaveLength(1);
+    expect(allFiles2).toHaveLength(3);
+  });
+});
