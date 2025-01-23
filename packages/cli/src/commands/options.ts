0a6151b8e6277ca0ff170cfc3651bde1dbb1ef30
Ken Chau <kchau@microsoft.com>
2025-01-23 23:58:21 +0000

    adding a new centralized place to define all possible options in various groupings.

---
+++ packages/cli/src/commands/options.ts
@@ -0,0 +1,68 @@
+import { Option } from "commander";
+
+const isCI = process.env.CI || process.env.TF_BUILD;
+
+export const options = {
+  logger: {
+    reporter: new Option("--reporter <reporter...>", "reporter"),
+    grouped: new Option("--grouped", "groups the logs").default(false),
+    progress: new Option("--progress").conflicts(["reporter", "grouped", "verbose"]).default(!isCI),
+    logLevel: new Option("--log-level <level>", "log level").choices(["info", "warn", "error", "verbose", "silly"]).conflicts("verbose"),
+    logFile: new Option("--log-file <file>", "when used with --reporter vfl, writes verbose, ungrouped logs to the specified file"),
+    verbose: new Option("--verbose", "verbose output").default(false),
+  },
+  pool: {
+    concurrency: new Option("-c|--concurrency <number>", "max jobs to run at a time").argParser((v) => parseInt(v)),
+    continue: new Option("--continue", "continue running even after encountering an error for one of the targets"),
+    nodeArg: new Option(
+      "-n|--node-arg <arg>",
+      "node argument to pass to worker, just a single string to be passed into node like a NODE_OPTIONS setting"
+    ),
+  },
+  run: {
+    noCache: new Option("--no-cache", "disables the cache"),
+    resetCache: new Option("--reset-cache", "resets the cache, filling it after a run"),
+    skipLocalCache: new Option("--skip-local-cache", "skips caching locally (defaults to true in CI environments)").default(isCI),
+    profile: new Option("--profile [profile]", "writes a run profile into a file that can be processed by Chromium devtool"),
+    continue: new Option("--continue", "continues the run even on error"),
+    allowNoTargetRuns: new Option("--allow-no-target-runs"),
+    watch: new Option("--watch", "runs in watch mode"),
+  },
+  info: {
+    server: new Option("--server [host:port]", "Run targets of type 'worker' on a background service"),
+  },
+  server: {
+    host: new Option("-h|--host <host>", "lage server host").default("localhost"),
+    port: new Option("-p|--port <port>", "lage worker server port").default(5332).argParser((v) => parseInt(v)),
+    timeout: new Option("-t|--timeout <seconds>", "lage server autoshutoff timeout").default(5 * 60).argParser((v) => parseInt(v)),
+    tasks: new Option("--tasks <tasks...>", "A list of tasks to run, separated by space e.g. 'build test'"),
+  },
+  filter: {
+    scope: new Option(
+      "--scope <scope...>",
+      "scopes the run to a subset of packages (by default, includes the dependencies and dependents as well)"
+    ),
+    noDeps: new Option("--no-deps|--no-dependents", "disables running any dependents of the scoped packages"),
+    includeDependencies: new Option(
+      "--include-dependencies|--dependencies",
+      'adds the scoped packages dependencies as the "entry points" for the target graph run'
+    ),
+    to: new Option("--to <scope...>", "runs up to a package (shorthand for --scope=<scope...> --no-dependents)"),
+    since: new Option("--since <since>", "only runs packages that have changed since the given commit, tag, or branch"),
+    ignore: new Option(
+      "--ignore <ignore...>",
+      "ignores files when calculating the scope with `--since` in addition to the files specified in lage.config"
+    ).default([]),
+  },
+  affected: {
+    outputFormat: new Option(
+      "--output-format <graph|json|default>",
+      `Generate a report about what packages are affected by the current change (defaults to human readable format) ` +
+        `"graph" will generate a GraphViz .dot file format`
+    ),
+  },
+  cache: {
+    cache: new Option("--cache", "cache the results of the run").default(false),
+    cacheDir: new Option("--cache-dir <dir>", "directory to store the cache").default(".lage-cache"),
+  },
+};
