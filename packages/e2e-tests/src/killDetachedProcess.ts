2ded0305e533e01e7ee1379621363aba986059b6
Danny van Velzen <dannyvv@microsoft.com>
2025-02-11 16:14:46 -0800

    Fix killDetachedProcess (#856)

--- packages/e2e-tests/src/killDetachedProcess.ts
+++ packages/e2e-tests/src/killDetachedProcess.ts
@@ -7,18 +7,7 @@ export function killDetachedProcess(pid: number | string) {
       process.kill(pidNumber, "SIGTERM");
     } else {
       // on Windows get the process tree and kill the parent
-      const tree = execSync(`wmic process where (ParentProcessId=${pidNumber}) get ProcessId`).toString();
-
-      const pids = tree
-        .split("\n")
-        .map((line: string) => line.trim())
-        .filter((line: string) => line !== "" && line !== "ProcessId");
-
-      pids.forEach((p: string) => {
-        process.kill(parseInt(p));
-      });
-
-      process.kill(pidNumber);
+      execSync(`taskkill /f /PID ${pidNumber} /T`);
     }
   } catch (e) {
     // ignore
