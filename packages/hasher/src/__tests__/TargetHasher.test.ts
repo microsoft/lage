d7fea6cd75a2a74d6b61c399448355a21fc48cd8
Dobes Vandermeer <dobes@formative.com>
2025-03-17 12:31:35 -0700

    Include filename and dep names in hash calculations
    If the name of a dependency or file changes, but the
    calculated hash does not, it should still result in a
    different hash.

--- packages/hasher/src/__tests__/TargetHasher.test.ts
+++ packages/hasher/src/__tests__/TargetHasher.test.ts
@@ -172,4 +172,24 @@ describe("The main Hasher class", () => {
 
     monorepo1.cleanup();
   });
+
+  it("creates different hashes when file path is different but file content is the same", async () => {
+    const content = "THIS IS CONTENT";
+    const monorepo1 = await setupFixture("monorepo-with-global-files");
+    const hasher = new TargetHasher({ root: monorepo1.root, environmentGlob: [] });
+    const target = createTarget(monorepo1.root, "package-a", "build");
+    target.inputs = ["file1.txt"];
+    fs.writeFileSync(path.join(monorepo1.root, "packages", "package-a", "file1.txt"), content);
+    const target2 = createTarget(monorepo1.root, "package-a", "build");
+    target2.inputs = ["file2.txt"];
+    fs.writeFileSync(path.join(monorepo1.root, "packages", "package-a", "file2.txt"), content);
+
+    const hash = await getHash(hasher, target);
+    const hash2 = await getHash(hasher, target2);
+
+    expect(hash).not.toEqual(hash2);
+
+    monorepo1.cleanup();
+  });
+
 });
