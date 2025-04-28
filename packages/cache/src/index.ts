8cd955ef4910abb9fff6f0de40255fe842847a79
Elizabeth Craig <elcraig@microsoft.com>
2025-04-28 15:53:56 -0700

    Remove duplicate cache options interface (#904)

--- packages/cache/src/index.ts
+++ packages/cache/src/index.ts
@@ -1,6 +1,6 @@
 export { BackfillCacheProvider } from "./providers/BackfillCacheProvider.js";
 export { RemoteFallbackCacheProvider } from "./providers/RemoteFallbackCacheProvider.js";
-export type { CacheOptions } from "./types/CacheOptions.js";
+export type { CacheOptions } from "@lage-run/config";
 export type { CacheProvider } from "./types/CacheProvider.js";
 
 export { getCacheDirectory, getLogsCacheDirectory, getCacheDirectoryRoot } from "./getCacheDirectory.js";
