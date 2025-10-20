927ae8a50588a0e3007d3b321f8c666a33d0801a
ntesic <nemanjatesic@microsoft.com>
2025-10-20 02:05:46 +0200

    Fix tests

--- packages/cli/jest.config.js
+++ packages/cli/jest.config.js
@@ -1,1 +1,4 @@
-module.exports = require("@lage-run/monorepo-scripts/config/jest.config.js");
+module.exports = {
+  ...require("@lage-run/monorepo-scripts/config/jest.config.js"),
+  testTimeout: process.platform === "win32" ? 20_000 : 10_000,
+};
