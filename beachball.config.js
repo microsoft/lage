092607a7d55561a9342ab3265ccab0142dd59b9b
Elizabeth Craig <elcraig@microsoft.com>
2026-01-24 00:26:09 -0800

    Add prepublish hook to unpin deps only for published version (#783)

--- beachball.config.js
+++ beachball.config.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 // @ts-check
 /** @type {import('beachball').BeachballConfig}*/
 const config = {
@@ -7,11 +9,27 @@ const config = {
     groups: [
       {
         // roll up all changes to the lage changelog (packages still have individual changelogs too)
-        masterPackageName: "lage",
+        mainPackageName: "lage",
         include: ["packages/*"],
         changelogPath: "packages/lage",
       },
     ],
+  },
+  hooks: {
+    prepublish: (packagePath, name, version, packageInfos) => {
+      const { packageJsonPath } = packageInfos[name];
+      const packageJson = require(packageJsonPath);
+      if (!packageJson.dependencies) return;
+
+      for (const [dep, version] of Object.entries(packageJson.dependencies)) {
+        // If the dep is a specific version, unpin before publishing.
+        // See the comment towards the end of renovate.json5 for why the deps are pinned to start out.
+        if (/\d/.test(version[0])) {
+          packageJson.dependencies[dep] = `^${version}`;
+        }
+      }
+      fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + "\n");
+    },
   },
   ignorePatterns: [".*ignore", "jest.config.js", "**/__*/**/*", "**/tests/**/*"],
   disallowedChangeTypes: ["major"],
