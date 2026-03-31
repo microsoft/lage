24007b6affbe53ab39e9061b16bca35139a90120
Elizabeth Craig <elcraig@microsoft.com>
2026-03-31 10:56:38 -0700

    fixes

--- scripts/config/jest-setup.js
+++ scripts/config/jest-setup.js
@@ -1,7 +1,10 @@
 process.env.FORCE_COLOR = "0";
 
+// Delete any backfill or lage environment variables from local or CI builds.
+// (Previously this called jest.resetModules() before and after every test, which causes unwanted
+// side effects and slows down tests. If the new approach proves inadequate in the future, any
+// refinements should also try to be as targeted as possible and avoid a global resetModules.)
 for (const key of Object.keys(process.env)) {
-  // Delete any backfill or lage environment variables from local or CI builds.
   if (key.startsWith("BACKFILL_") || key.startsWith("LAGE_")) {
     delete process.env[key];
   }
