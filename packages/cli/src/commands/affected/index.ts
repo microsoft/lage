ce44a8e432bf8b251bf65265f9dbdf47073e4994
lukatomanovic <82061113+lukatomanovic@users.noreply.github.com>
2026-06-12 22:18:12 +0200

    Expose the --output-format option on the affected command (#1148)
    Co-authored-by: lutomanovic_microsoft <lutomanovic@microsoft.com>

--- packages/cli/src/commands/affected/index.ts
+++ packages/cli/src/commands/affected/index.ts
@@ -3,6 +3,10 @@ import { affectedAction } from "./action.js";
 import { addOptions } from "../addOptions.js";
 
 const affectedCommand: Command = new Command("affected");
-addOptions("filter", affectedCommand).action(affectedAction);
+
+addOptions("filter", affectedCommand);
+addOptions("affected", affectedCommand);
+
+affectedCommand.action(affectedAction);
 
 export { affectedCommand };
